2d04efe8631443e9cb6dc0852ca5e16b5e09be09
Christophe Fargette <jfargett@redhat.com>
2023-02-08 22:42:55 -0500

    Fix indentations

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Navigate, Route } from 'react-router-dom';
-import { apiDocsPlugin, ApiExplorerPage } from '@backstage/plugin-api-docs';
+import {Navigate, Route} from 'react-router-dom';
+import {apiDocsPlugin, ApiExplorerPage} from '@backstage/plugin-api-docs';
 import {
   CatalogEntityPage,
   CatalogIndexPage,
@@ -10,35 +10,35 @@ import {
   CatalogImportPage,
   catalogImportPlugin,
 } from '@backstage/plugin-catalog-import';
-import { ScaffolderPage, scaffolderPlugin } from '@backstage/plugin-scaffolder';
-import { orgPlugin } from '@backstage/plugin-org';
-import { SearchPage } from '@backstage/plugin-search';
-import { TechRadarPage } from '@backstage/plugin-tech-radar';
+import {ScaffolderPage, scaffolderPlugin} from '@backstage/plugin-scaffolder';
+import {orgPlugin} from '@backstage/plugin-org';
+import {SearchPage} from '@backstage/plugin-search';
+import {TechRadarPage} from '@backstage/plugin-tech-radar';
 import {
   TechDocsIndexPage,
   techdocsPlugin,
   TechDocsReaderPage,
 } from '@backstage/plugin-techdocs';
-import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
-import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
-import { UserSettingsPage } from '@backstage/plugin-user-settings';
-import { apis } from './apis';
-import { entityPage } from './components/catalog/EntityPage';
-import { searchPage } from './components/search/SearchPage';
-import { Root } from './components/Root';
+import {TechDocsAddons} from '@backstage/plugin-techdocs-react';
+import {ReportIssue} from '@backstage/plugin-techdocs-module-addons-contrib';
+import {UserSettingsPage} from '@backstage/plugin-user-settings';
+import {apis} from './apis';
+import {entityPage} from './components/catalog/EntityPage';
+import {searchPage} from './components/search/SearchPage';
+import {Root} from './components/Root';
 
-import { AlertDisplay, OAuthRequestDialog } from '@backstage/core-components';
-import { createApp } from '@backstage/app-defaults';
-import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
-import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
-import { RequirePermission } from '@backstage/plugin-permission-react';
-import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
+import {AlertDisplay, OAuthRequestDialog} from '@backstage/core-components';
+import {createApp} from '@backstage/app-defaults';
+import {AppRouter, FlatRoutes} from '@backstage/core-app-api';
+import {CatalogGraphPage} from '@backstage/plugin-catalog-graph';
+import {RequirePermission} from '@backstage/plugin-permission-react';
+import {catalogEntityCreatePermission} from '@backstage/plugin-catalog-common/alpha';
 import {CssBaseline, ThemeProvider} from "@material-ui/core";
-import { janusTheme } from './themes/janus';
+import {janusTheme} from './themes/janus';
 
 const app = createApp({
   apis,
-  bindRoutes({ bind }) {
+  bindRoutes({bind}) {
     bind(catalogPlugin.externalRoutes, {
       createComponent: scaffolderPlugin.routes.root,
       viewTechDoc: techdocsPlugin.routes.docRoot,
@@ -58,7 +58,7 @@ const app = createApp({
       id: 'janus',
       title: 'Janus Theme',
       variant: 'light',
-      Provider: ({ children }) => (
+      Provider: ({children}) => (
         <ThemeProvider theme={janusTheme}>
           <CssBaseline>{children}</CssBaseline>
         </ThemeProvider>
@@ -69,49 +69,49 @@ const app = createApp({
 
 const routes = (
   <FlatRoutes>
-    <Route path="/" element={<Navigate to="catalog" />} />
-    <Route path="/catalog" element={<CatalogIndexPage />} />
+    <Route path="/" element={<Navigate to="catalog"/>}/>
+    <Route path="/catalog" element={<CatalogIndexPage/>}/>
     <Route
       path="/catalog/:namespace/:kind/:name"
-      element={<CatalogEntityPage />}
+      element={<CatalogEntityPage/>}
     >
       {entityPage}
     </Route>
-    <Route path="/docs" element={<TechDocsIndexPage />} />
+    <Route path="/docs" element={<TechDocsIndexPage/>}/>
     <Route
       path="/docs/:namespace/:kind/:name/*"
-      element={<TechDocsReaderPage />}
+      element={<TechDocsReaderPage/>}
     >
       <TechDocsAddons>
-        <ReportIssue />
+        <ReportIssue/>
       </TechDocsAddons>
     </Route>
-    <Route path="/create" element={<ScaffolderPage />} />
-    <Route path="/api-docs" element={<ApiExplorerPage />} />
+    <Route path="/create" element={<ScaffolderPage/>}/>
+    <Route path="/api-docs" element={<ApiExplorerPage/>}/>
     <Route
       path="/tech-radar"
-      element={<TechRadarPage width={1500} height={800} />}
+      element={<TechRadarPage width={1500} height={800}/>}
     />
     <Route
       path="/catalog-import"
       element={
         <RequirePermission permission={catalogEntityCreatePermission}>
-          <CatalogImportPage />
+          <CatalogImportPage/>
         </RequirePermission>
       }
     />
-    <Route path="/search" element={<SearchPage />}>
+    <Route path="/search" element={<SearchPage/>}>
       {searchPage}
     </Route>
-    <Route path="/settings" element={<UserSettingsPage />} />
-    <Route path="/catalog-graph" element={<CatalogGraphPage />} />
+    <Route path="/settings" element={<UserSettingsPage/>}/>
+    <Route path="/catalog-graph" element={<CatalogGraphPage/>}/>
   </FlatRoutes>
 );
 
 export default app.createRoot(
   <>
-    <AlertDisplay />
-    <OAuthRequestDialog />
+    <AlertDisplay/>
+    <OAuthRequestDialog/>
     <AppRouter>
       <Root>{routes}</Root>
     </AppRouter>
--- packages/app/src/themes/janus.ts
+++ packages/app/src/themes/janus.ts
@@ -1,35 +1,35 @@
 import {BackstageTheme, createTheme, genPageTheme, lightTheme, shapes} from '@backstage/theme';
 
 const baseTheme = createTheme({
-    palette: {
-        ...lightTheme.palette,
-        navigation: {
-            background: '#222427',
-            indicator: '#009596',
-            color: '#ffffff',
-            selectedColor: '#ffffff',
-            navItem: {
-                hoverBackground: '#4f5255',
-            },
-        },
+  palette: {
+    ...lightTheme.palette,
+    navigation: {
+      background: '#222427',
+      indicator: '#009596',
+      color: '#ffffff',
+      selectedColor: '#ffffff',
+      navItem: {
+        hoverBackground: '#4f5255',
+      },
     },
-    defaultPageTheme: 'home',
-    pageTheme: {
-        home: genPageTheme({ colors: ['#005f60', '#73c5c5'], shape: shapes.wave }),
-        app: genPageTheme({ colors: ['#005f60', '#73c5c5'], shape: shapes.wave }),
-        apis: genPageTheme({ colors: ['#005f60', '#73c5c5'], shape: shapes.wave }),
-        documentation: genPageTheme({
-            colors: ['#005f60', '#73c5c5'],
-            shape: shapes.wave,
-        }),
-        tool: genPageTheme({ colors: ['#005f60', '#73c5c5'], shape: shapes.round }),
-        other: genPageTheme({ colors: ['#005f60', '#73c5c5'], shape: shapes.wave }),
-    },
+  },
+  defaultPageTheme: 'home',
+  pageTheme: {
+    home: genPageTheme({colors: ['#005f60', '#73c5c5'], shape: shapes.wave}),
+    app: genPageTheme({colors: ['#005f60', '#73c5c5'], shape: shapes.wave}),
+    apis: genPageTheme({colors: ['#005f60', '#73c5c5'], shape: shapes.wave}),
+    documentation: genPageTheme({
+      colors: ['#005f60', '#73c5c5'],
+      shape: shapes.wave,
+    }),
+    tool: genPageTheme({colors: ['#005f60', '#73c5c5'], shape: shapes.round}),
+    other: genPageTheme({colors: ['#005f60', '#73c5c5'], shape: shapes.wave}),
+  },
 });
 
 export const janusTheme: BackstageTheme = {
-    ...baseTheme,
-    overrides: {
-        ...baseTheme.overrides,
-    },
+  ...baseTheme,
+  overrides: {
+    ...baseTheme.overrides,
+  },
 };
