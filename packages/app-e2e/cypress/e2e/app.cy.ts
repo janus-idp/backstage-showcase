ec4a8d6c294e9ec5d35781970abd82af0514101c
Christophe Fargette <jfargett@redhat.com>
2023-02-14 16:49:17 -0500

    Update test

--- packages/app-e2e/cypress/e2e/app.cy.ts
+++ packages/app-e2e/cypress/e2e/app.cy.ts
@@ -1,6 +1,9 @@
 describe('App', () => {
   it('should render the catalog', () => {
     cy.visit('/');
+    cy.contains('Quick Access');
+
+    cy.visit('/catalog');
     cy.contains('Red Hat Catalog');
   });
 });
