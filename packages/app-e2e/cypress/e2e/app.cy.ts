061829f6f58cfaa746530ed8efaae83989a2236e
Patrick Knight <pknight@redhat.com>
2023-03-31 09:33:40 -0400

    Remove e2e directory

--- packages/app-e2e/cypress/e2e/app.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('App', () => {
-  it('should render the catalog', () => {
-    cy.visit('/');
-    cy.contains('Quick Access');
-
-    cy.visit('/catalog');
-    cy.contains('Janus IDP Catalog');
-  });
-});
