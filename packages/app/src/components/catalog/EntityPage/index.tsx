4eef16f506a81ada9328c0c1f0211e80ff4ddfbc
Paul Schultz <pschultz@pobox.com>
2023-04-25 13:34:47 -0500

    Fix tsc errors
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- packages/app/src/components/catalog/EntityPage/index.tsx
+++ packages/app/src/components/catalog/EntityPage/index.tsx
@@ -15,12 +15,12 @@ import {
 export const entityPage = (
   <EntitySwitch>
     <EntitySwitch.Case if={isKind('component')} children={componentPage} />
-    <EntitySwitch.Case if={isKind('api')} children={<apiPage />} />
-    <EntitySwitch.Case if={isKind('group')} children={<groupPage />} />
-    <EntitySwitch.Case if={isKind('user')} children={<userPage />} />
-    <EntitySwitch.Case if={isKind('system')} children={<systemPage />} />
-    <EntitySwitch.Case if={isKind('domain')} children={<domainPage />} />
-    <EntitySwitch.Case if={isKind('resource')} children={<resourcePage />} />
+    <EntitySwitch.Case if={isKind('api')} children={apiPage} />
+    <EntitySwitch.Case if={isKind('group')} children={groupPage} />
+    <EntitySwitch.Case if={isKind('user')} children={userPage} />
+    <EntitySwitch.Case if={isKind('system')} children={systemPage} />
+    <EntitySwitch.Case if={isKind('domain')} children={domainPage} />
+    <EntitySwitch.Case if={isKind('resource')} children={resourcePage} />
 
     <EntitySwitch.Case>{defaultEntityPage}</EntitySwitch.Case>
   </EntitySwitch>
