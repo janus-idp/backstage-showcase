fc13f071e65119bf371bf5a75558935f83462698
debsmita1 <debsmita.santra@gmail.com>
2023-05-08 23:25:05 +0530

    add the annotation to enable tekton in the CI tab of Backstage Showcase entity

--- packages/app/src/components/catalog/EntityPage/Content/CI.tsx
+++ packages/app/src/components/catalog/EntityPage/Content/CI.tsx
@@ -21,9 +21,33 @@ const ifCIs: ((e: Entity) => boolean)[] = [
   isTektonCIAvailable,
 ];
 
+const ifTektonAndGitlabCIs: ((e: Entity) => boolean)[] = [
+  isGitlabAvailable,
+  isTektonCIAvailable,
+];
+
+const ifTektonAndGithubActionsCIs: ((e: Entity) => boolean)[] = [
+  isGithubActionsAvailable,
+  isTektonCIAvailable,
+];
+
+const ifGithubQActionsAndGitlabCIs: ((e: Entity) => boolean)[] = [
+  isGitlabAvailable,
+  isGithubActionsAvailable,
+];
+
 export const isCIsAvailable = (e: Entity) => ifCIs.some(f => f(e));
 
 export const areAllCIsAvailable = (e: Entity) => ifCIs.every(f => f(e));
+
+export const areGithubActionsAndGitlabCIsAvailable = (e: Entity) =>
+  ifGithubQActionsAndGitlabCIs.every(f => f(e));
+
+export const areGithubActionsAndTektonCIsAvailable = (e: Entity) =>
+  ifTektonAndGithubActionsCIs.every(f => f(e));
+
+export const areTektonAndGitlabCIsAvailable = (e: Entity) =>
+  ifTektonAndGitlabCIs.every(f => f(e));
 
 export const ciContent = (
   <Grid container spacing={3} justifyContent="space-evenly">
@@ -32,6 +56,33 @@ export const ciContent = (
         <Grid item xs={12}>
           <EntityGithubActionsContent />
         </Grid>
+        <Grid item xs={12}>
+          <EntityGitlabMergeRequestsTable />
+        </Grid>
+        <Grid item xs={12}>
+          <LatestPipelineRun linkTekton />
+        </Grid>
+      </EntitySwitch.Case>
+
+      <EntitySwitch.Case if={areGithubActionsAndGitlabCIsAvailable}>
+        <Grid item xs={12}>
+          <EntityGithubActionsContent />
+        </Grid>
+        <Grid item xs={12}>
+          <EntityGitlabMergeRequestsTable />
+        </Grid>
+      </EntitySwitch.Case>
+
+      <EntitySwitch.Case if={areGithubActionsAndTektonCIsAvailable}>
+        <Grid item xs={12}>
+          <EntityGithubActionsContent />
+        </Grid>
+        <Grid item xs={12}>
+          <LatestPipelineRun linkTekton />
+        </Grid>
+      </EntitySwitch.Case>
+
+      <EntitySwitch.Case if={areTektonAndGitlabCIsAvailable}>
         <Grid item xs={12}>
           <EntityGitlabMergeRequestsTable />
         </Grid>
