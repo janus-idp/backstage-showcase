dc653e1bbe2ff642949b00e2eb2cc2f179664a85
Paul Schultz <pschultz@pobox.com>
2023-05-26 14:10:57 -0500

    fix issues tab (#266)
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import { createApp } from '@backstage/app-defaults';
+import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
+import { AlertDisplay, OAuthRequestDialog } from '@backstage/core-components';
 import { apiDocsPlugin, ApiExplorerPage } from '@backstage/plugin-api-docs';
 import {
   CatalogEntityPage,
   CatalogIndexPage,
   catalogPlugin,
 } from '@backstage/plugin-catalog';
+import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
+import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import {
   CatalogImportPage,
   catalogImportPlugin,
 } from '@backstage/plugin-catalog-import';
-import { ScaffolderPage, scaffolderPlugin } from '@backstage/plugin-scaffolder';
+import { HomepageCompositionRoot } from '@backstage/plugin-home';
 import { orgPlugin } from '@backstage/plugin-org';
+import { RequirePermission } from '@backstage/plugin-permission-react';
+import { ScaffolderPage, scaffolderPlugin } from '@backstage/plugin-scaffolder';
 import { SearchPage as BackstageSearchPage } from '@backstage/plugin-search';
 import { TechRadarPage } from '@backstage/plugin-tech-radar';
 import {
@@ -19,25 +24,20 @@ import {
   techdocsPlugin,
   TechDocsReaderPage,
 } from '@backstage/plugin-techdocs';
-import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
+import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { UserSettingsPage } from '@backstage/plugin-user-settings';
-import { AlertDisplay, OAuthRequestDialog } from '@backstage/core-components';
-import { createApp } from '@backstage/app-defaults';
-import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
-import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
-import { RequirePermission } from '@backstage/plugin-permission-react';
-import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
-import { CssBaseline, ThemeProvider } from '@material-ui/core';
-import { HomepageCompositionRoot } from '@backstage/plugin-home';
 import { OcmPage } from '@janus-idp/backstage-plugin-ocm';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import React from 'react';
+import { Route } from 'react-router-dom';
 import { apis } from './apis';
-import Logo from './components/Root/LogoIcon';
-import { LearningPaths } from './components/learningPaths/LearningPathsPage';
 import { entityPage } from './components/catalog/EntityPage';
-import { SearchPage } from './components/search/SearchPage';
-import { Root } from './components/Root';
 import { HomePage } from './components/home/HomePage';
+import { LearningPaths } from './components/learningPaths/LearningPathsPage';
+import { Root } from './components/Root';
+import Logo from './components/Root/LogoIcon';
+import { SearchPage } from './components/search/SearchPage';
 import { janusTheme } from './themes/janus';
 
 const app = createApp({
@@ -78,7 +78,6 @@ const routes = (
     <Route path="/" element={<HomepageCompositionRoot />}>
       <HomePage />
     </Route>
-    ;
     <Route path="/catalog" element={<CatalogIndexPage />} />
     <Route
       path="/catalog/:namespace/:kind/:name"
