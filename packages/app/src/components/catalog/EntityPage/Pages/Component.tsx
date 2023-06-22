fa72691acc46e245b14d76015ad099140e12934b
christophe-f <christophe-f@users.noreply.github.com>
2023-06-22 14:57:12 -0400

    UX updates (#345)

--- packages/app/src/components/catalog/EntityPage/Pages/Component.tsx
+++ packages/app/src/components/catalog/EntityPage/Pages/Component.tsx
@@ -26,7 +26,6 @@ import {
   monitoringContent,
   overviewContent,
   prContent,
-  securityContent,
   techdocsContent,
 } from '../Content';
 import { defaultEntityPage } from './DefaultEntity';
@@ -35,6 +34,10 @@ const componentEntityPage = (componentType: 'service' | 'website') => (
   <EntityLayout>
     <EntityLayout.Route path="/" title="Overview">
       {overviewContent}
+    </EntityLayout.Route>
+
+    <EntityLayout.Route path="/topology" title="Topology">
+      <TopologyPage />
     </EntityLayout.Route>
 
     <EntityLayout.Route if={isIssuesAvailable} path="/issues" title="Issues">
@@ -62,10 +65,6 @@ const componentEntityPage = (componentType: 'service' | 'website') => (
       <EntityKubernetesContent refreshIntervalMs={30000} />
     </EntityLayout.Route>
 
-    <EntityLayout.Route path="/topology" title="Topology">
-      <TopologyPage />
-    </EntityLayout.Route>
-
     <EntityLayout.Route path="/tekton" title="Tekton">
       <TektonPage />
     </EntityLayout.Route>
@@ -76,10 +75,6 @@ const componentEntityPage = (componentType: 'service' | 'website') => (
       title="Image Registry"
     >
       {imageRegistry}
-    </EntityLayout.Route>
-
-    <EntityLayout.Route path="/security-insights" title="Security Insights">
-      {securityContent}
     </EntityLayout.Route>
 
     <EntityLayout.Route
