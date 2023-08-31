9ce0bc8958be92e6cee0806c8881b572f3290cfd
Jason Froehlich <jfroehli@redhat.com>
2023-08-31 16:37:05 -0400

    Revert "Added Spectral plugin"
    This reverts commit 1432011f4eccd6706c5c48e8844a7e1b1b2ff5f4.

--- packages/app/src/components/catalog/EntityPage/Pages/Api.tsx
+++ packages/app/src/components/catalog/EntityPage/Pages/Api.tsx
@@ -12,7 +12,6 @@ import { EntityCatalogGraphCard } from '@backstage/plugin-catalog-graph';
 import Grid from '@mui/material/Grid';
 import React from 'react';
 import { entityWarningContent } from '../Content/EntityWarning';
-import { EntityApiDocsSpectralLinterContent, isApiDocsSpectralLinterAvailable } from '@dweber019/backstage-plugin-api-docs-spectral-linter';
 
 export const apiPage = (
   <EntityLayout>
@@ -60,10 +59,6 @@ export const apiPage = (
           <EntityApiDefinitionCard />
         </Grid>
       </Grid>
-    </EntityLayout.Route>
-
-    <EntityLayout.Route if={isApiDocsSpectralLinterAvailable} path="/linter" title="Linter">
-      <EntityApiDocsSpectralLinterContent />
     </EntityLayout.Route>
   </EntityLayout>
 );
