a6f3a6b1cda53588bc8357dd1d449d84825427c7
David Festal <dfestal@redhat.com>
2023-11-02 18:47:30 +0100

    fix: disable cache for injected file.
    Backed merged config is inject inside one of the Javascript
    static assets.
    We should never allow this file to be cached on the client side,
    so that any change in the configuration can be seen on the frontend
    side after a backend restart.
    That's particularly critical to get the latest UI configuration
    of added dynamic frontend plugins.
    
    Signed-off-by: David Festal <dfestal@redhat.com>

--- packages/backend/src/plugins/app.ts
+++ packages/backend/src/plugins/app.ts
@@ -1,14 +1,57 @@
 import { createRouter } from '@backstage/plugin-app-backend';
-import type { Router } from 'express';
+import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
+import { resolvePackagePath } from '@backstage/backend-common';
+import { resolve as resolvePath } from 'path';
+import fs from 'fs-extra';
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
-  return await createRouter({
+  const appPackageName = 'app';
+
+  const appDistDir = resolvePackagePath(appPackageName, 'dist');
+  const staticDir = resolvePath(appDistDir, 'static');
+
+  const files = await fs.readdir(staticDir);
+  const jsFiles = files.filter(file => file.endsWith('.js'));
+  let injectedJSFile: string | undefined = undefined;
+
+  for (const jsFile of jsFiles) {
+    const path = resolvePath(staticDir, jsFile);
+
+    const content = await fs.readFile(path, 'utf8');
+    if (content.includes('__APP_INJECTED_')) {
+      injectedJSFile = jsFile;
+      break;
+    }
+  }
+
+  const router = await createRouter({
     logger: env.logger,
     config: env.config,
     database: env.database,
-    appPackageName: 'app',
+    appPackageName,
   });
+
+  const enclosingRouter = Router();
+  if (injectedJSFile) {
+    env.logger.info(
+      `Setting up static router for injected Javascript file ${injectedJSFile}`,
+    );
+
+    enclosingRouter.get(`/static/${injectedJSFile}`, (_req, res) => {
+      env.logger.info(
+        `Serving in the injected Javascript file with caching disabled`,
+      );
+      res.sendFile(resolvePath(staticDir, injectedJSFile!), {
+        headers: {
+          'cache-control': 'no-store, max-age=0',
+        },
+      });
+    });
+  }
+
+  enclosingRouter.use(router);
+  return enclosingRouter;
 }
