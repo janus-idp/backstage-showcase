7dffd68f8ac2b80c1c7ff042f5571555fb7113ee
Tomas Coufal <tcoufal@redhat.com>
2023-11-08 16:35:08 +0100

    fix: bring back responsive layout
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- packages/app/src/components/catalog/EntityPage/index.tsx
+++ packages/app/src/components/catalog/EntityPage/index.tsx
@@ -85,71 +85,212 @@ export const entityPage = (
               </Grid>
             </EntitySwitch.Case>
           </EntitySwitch>
-          <EntitySwitch>
-            <EntitySwitch.Case if={hasLinks}>
-              <Grid item sx={{ gridColumn: '1 / span 4' }}>
-                <EntityLinksCard />
+          <Grid
+            item
+            sx={{
+              gridColumn: {
+                lg: '1 / span 4',
+                md: '1 / span 6',
+                xs: '1 / -1',
+              },
+              gridRowEnd: 'span 2',
+            }}
+          >
+            <Grid container>
+              <EntitySwitch>
+                <EntitySwitch.Case if={hasLinks}>
+                  <Grid item sx={{ gridColumn: '1 / -1' }}>
+                    <EntityLinksCard />
+                  </Grid>
+                </EntitySwitch.Case>
+              </EntitySwitch>
+              <Grid item sx={{ gridColumn: '1 / -1' }}>
+                <EntityAboutCard />
               </Grid>
-            </EntitySwitch.Case>
-          </EntitySwitch>
-          <Grid item sx={{ gridColumn: '1 / span 4' }}>
-            <EntityAboutCard />
+            </Grid>
           </Grid>
           <EntitySwitch>
             <EntitySwitch.Case if={isKind('domain')}>
-              <Grid item sx={{ gridColumn: '5 / -1', gridRowEnd: 'span 4' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '5 / -1',
+                    md: '7 / -1',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityCatalogGraphCard variant="gridItem" height={400} />
               </Grid>
-              <Grid item sx={{ gridColumn: '5 / -1' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '5 / -1',
+                    md: '7 / -1',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityHasSystemsCard variant="gridItem" />
               </Grid>
             </EntitySwitch.Case>
             <EntitySwitch.Case if={isKind('group')}>
-              <Grid item sx={{ gridColumn: '5 / -1' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '5 / -1',
+                    md: '7 / -1',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityGroupProfileCard variant="gridItem" />
               </Grid>
-              <Grid item sx={{ gridColumn: '5 / -1' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '5 / -1',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityOwnershipCard variant="gridItem" />
               </Grid>
-              <Grid item sx={{ gridColumn: '5 / -1' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '5 / -1',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityMembersListCard />
               </Grid>
             </EntitySwitch.Case>
             <EntitySwitch.Case if={isKind('user')}>
-              <Grid item sx={{ gridColumn: '5 / -1' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '5 / -1',
+                    md: '7 / -1',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityUserProfileCard variant="gridItem" />
               </Grid>
-              <Grid item sx={{ gridColumn: '5 / -1' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '5 / -1',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityOwnershipCard variant="gridItem" />
               </Grid>
             </EntitySwitch.Case>
             <EntitySwitch.Case if={isKind('api')}>
-              <Grid item sx={{ gridColumn: '5 / -1', gridRowEnd: 'span 4' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '5 / -1',
+                    md: '7 / -1',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityCatalogGraphCard variant="gridItem" height={400} />
               </Grid>
-              <Grid item sx={{ gridColumn: '1 / span 6' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '1 / span 6',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityProvidingComponentsCard />
               </Grid>
-              <Grid item sx={{ gridColumn: '7 / span 6' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '7 / span 6',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityConsumingComponentsCard />
               </Grid>
             </EntitySwitch.Case>
             <EntitySwitch.Case if={isKind('system')}>
-              <Grid item sx={{ gridColumn: '5 / -1', gridRowEnd: 'span 4' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '5 / -1',
+                    md: '7 / -1',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityCatalogGraphCard variant="gridItem" height={400} />
               </Grid>
-              <Grid item sx={{ gridColumn: '5 / -1' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '5 / -1',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityHasComponentsCard />
               </Grid>
-              <Grid item sx={{ gridColumn: '1 / span 6' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '1 / span 6',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityHasApisCard />
               </Grid>
-              <Grid item sx={{ gridColumn: '7 / span 6' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '7 / span 6',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityHasResourcesCard />
               </Grid>
             </EntitySwitch.Case>
             <EntitySwitch.Case if={isKind('resource')}>
-              <Grid item sx={{ gridColumn: '5 / -1', gridRowEnd: 'span 4' }}>
+              <Grid
+                item
+                sx={{
+                  gridColumn: {
+                    lg: '5 / -1',
+                    md: '7 / -1',
+                    xs: '1 / -1',
+                  },
+                }}
+              >
                 <EntityCatalogGraphCard variant="gridItem" height={400} />
               </Grid>
             </EntitySwitch.Case>
@@ -228,10 +369,26 @@ export const entityPage = (
           <EntitySwitch.Case
             if={e => isType('service')(e) && isKind('component')(e)}
           >
-            <Grid item sx={{ gridColumn: '1 / 7' }}>
+            <Grid
+              item
+              sx={{
+                gridColumn: {
+                  lg: '1 / span 6',
+                  xs: '1 / -1',
+                },
+              }}
+            >
               <EntityProvidedApisCard />
             </Grid>
-            <Grid item sx={{ gridColumn: '7 / -1' }}>
+            <Grid
+              item
+              sx={{
+                gridColumn: {
+                  lg: '7 / span 6',
+                  xs: '1 / -1',
+                },
+              }}
+            >
               <EntityConsumedApisCard />
             </Grid>
           </EntitySwitch.Case>
@@ -247,26 +404,75 @@ export const entityPage = (
       children: (
         <EntitySwitch>
           <EntitySwitch.Case if={isKind('component')}>
-            <Grid item sx={{ gridRow: '1 / 6', gridColumn: '1 / 7' }}>
+            <Grid
+              item
+              sx={{
+                gridColumn: {
+                  lg: '1 / span 6',
+                  xs: '1 / -1',
+                },
+                gridRowEnd: 'span 6',
+              }}
+            >
               <EntityCatalogGraphCard
                 variant="gridItem"
                 direction={Direction.TOP_BOTTOM}
                 height={900}
               />
             </Grid>
-            <Grid item sx={{ gridColumn: '7 / -1' }}>
+            <Grid
+              item
+              sx={{
+                gridColumn: {
+                  lg: '7 / -1',
+                  xs: '1 / -1',
+                },
+              }}
+            >
               <EntityDependsOnComponentsCard variant="gridItem" />
             </Grid>
-            <Grid item sx={{ gridColumn: '7 / -1' }}>
+            <Grid
+              item
+              sx={{
+                gridColumn: {
+                  lg: '7 / -1',
+                  xs: '1 / -1',
+                },
+              }}
+            >
               <EntityDependsOnResourcesCard variant="gridItem" />
             </Grid>
-            <Grid item sx={{ gridColumn: '7 / -1' }}>
+            <Grid
+              item
+              sx={{
+                gridColumn: {
+                  lg: '7 / -1',
+                  xs: '1 / -1',
+                },
+              }}
+            >
               <EntityHasSubcomponentsCard variant="gridItem" />
             </Grid>
-            <Grid item sx={{ gridColumn: '7 / -1' }}>
+            <Grid
+              item
+              sx={{
+                gridColumn: {
+                  lg: '7 / -1',
+                  xs: '1 / -1',
+                },
+              }}
+            >
               <EntityProvidedApisCard />
             </Grid>
-            <Grid item sx={{ gridColumn: '7 / -1' }}>
+            <Grid
+              item
+              sx={{
+                gridColumn: {
+                  lg: '7 / -1',
+                  xs: '1 / -1',
+                },
+              }}
+            >
               <EntityConsumedApisCard />
             </Grid>
           </EntitySwitch.Case>
