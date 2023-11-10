fe8ed10832ea2b72800992eb6a444d82f2b8e497
Tom Coufal <7453394+tumido@users.noreply.github.com>
2023-11-10 15:38:54 +0100

    fix: bring back responsive layout (#738)
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>
    Co-authored-by: Tomas Kral <tkral@redhat.com>

--- packages/app/src/components/catalog/tab/tab.tsx
+++ packages/app/src/components/catalog/tab/tab.tsx
@@ -3,6 +3,7 @@ import getMountPointData from '../../../utils/dynamicUI/getMountPointData';
 import { EntityLayout, EntitySwitch } from '@backstage/plugin-catalog';
 import Grid from '../Grid';
 import { Entity } from '@backstage/catalog-model';
+import Box from '@mui/material/Box';
 
 export type TabProps = {
   path: string;
@@ -39,9 +40,9 @@ const tab = ({
           ({ component: Component, config }) => (
             <EntitySwitch key={`${Component.displayName}`}>
               <EntitySwitch.Case if={config.if}>
-                <div style={config.layout}>
+                <Box sx={config.layout}>
                   <Component {...config.props} />
-                </div>
+                </Box>
               </EntitySwitch.Case>
             </EntitySwitch>
           ),
