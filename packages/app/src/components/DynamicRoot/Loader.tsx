b594d80902e7632b796d8945fcc955103f3fa0e5
Tom Coufal <7453394+tumido@users.noreply.github.com>
2023-11-13 18:45:09 +0100

    fix: the Loader component to respect theme (#758)
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>
    Co-authored-by: Tomas Kral <tkral@redhat.com>

--- packages/app/src/components/DynamicRoot/Loader.tsx
+++ packages/app/src/components/DynamicRoot/Loader.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { customDarkTheme } from '../../themes/darkTheme';
+import { customLightTheme } from '../../themes/lightTheme';
+
+const themeLight = createTheme({
+  palette: customLightTheme({}).getTheme('v5')?.palette,
+});
+
+const themeDark = createTheme({
+  palette: customDarkTheme({}).getTheme('v5')?.palette,
+});
 
 const Loader = () => {
+  // Hack access theme context before Backstage App is even instantiated
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    setTheme(prevTheme => localStorage.getItem('theme') || prevTheme);
+  }, [theme]);
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 'calc(100vw - 16px)',
-        height: 'calc(100vh - 16px)',
-        backgroundColor: '#F8F8F8',
-      }}
-    >
-      <CircularProgress />
-    </Box>
+    <ThemeProvider theme={theme === 'dark' ? themeDark : themeLight}>
+      <CssBaseline />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '100vh',
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    </ThemeProvider>
   );
 };
 
