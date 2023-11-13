28464d2b00fd3d2aa3dc4dcaf1cd518b2ea666cb
Joseph Kim <joskim@redhat.com>
2023-11-13 05:36:34 -0500

    RHIDP-512 - adapt new helm chart (#753)
    * adapt to use new helm chart
    
    Signed-off-by: Joseph Kim <joskim@redhat.com>
    
    * adapt to use new helm chart
    
    Signed-off-by: Joseph Kim <joskim@redhat.com>
    
    * adapt to use new helm chart
    
    Signed-off-by: Joseph Kim <joskim@redhat.com>
    
    * adapt to use new helm chart
    
    Signed-off-by: Joseph Kim <joskim@redhat.com>
    
    ---------
    
    Signed-off-by: Joseph Kim <joskim@redhat.com>
    Co-authored-by: Tomas Kral <tkral@redhat.com>

--- .ibm/pipelines/openshift-tests.sh
+++ .ibm/pipelines/openshift-tests.sh
@@ -2,51 +2,39 @@
 
 set -e
 
-function cleanup {
-    echo "Cleaning up before exiting"
-    helm uninstall ${RELEASE_NAME} -n ${NAME_SPACE}
-    oc delete namespace ${NAME_SPACE}
-    rm -rf ~/tmpbin
+# Variáveis globais
+LOGFILE="pr-${GIT_PR_NUMBER}-openshift-tests-${BUILD_NUMBER}"
+TEST_NAME="backstage-showcase Tests"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cleanup() {
+  echo "Cleaning up before exiting"
+  helm uninstall ${RELEASE_NAME} -n ${NAME_SPACE}
+  oc delete namespace ${NAME_SPACE}
+  rm -rf ~/tmpbin
 }
 
 trap cleanup EXIT
 
 add_helm_repos() {
-    # check installed helm version
-    helm version
+  helm version
 
-    # Check if the bitnami repository already exists
-    if ! helm repo list | grep -q "^bitnami"; then
-        helm repo add bitnami https://charts.bitnami.com/bitnami
-    else
-        echo "Repository bitnami already exists - updating repository instead."
-    fi
+  declare -a repos=("bitnami=https://charts.bitnami.com/bitnami" "backstage=https://backstage.github.io/charts" "janus-idp=https://janus-idp.github.io/helm-backstage" "${HELM_REPO_NAME}=${HELM_REPO_URL}")
 
-    # Check if the backstage repository already exists
-    if ! helm repo list | grep -q "^backstage"; then
-        helm repo add backstage https://backstage.github.io/charts
-    else
-        echo "Repository backstage already exists - updating repository instead."
-    fi
-    
-    # Check if the backstage repository already exists
-    if ! helm repo list | grep -q "^janus-idp"; then
-        helm repo add janus-idp https://janus-idp.github.io/helm-backstage
-    else
-        echo "Repository janus-idp already exists - updating repository instead."
-    fi
-    
-    # Check if the repository already exists
-    if ! helm repo list | grep -q "^${HELM_REPO_NAME}"; then
-        helm repo add "${HELM_REPO_NAME}" "${HELM_REPO_URL}"
-    else
-        echo "Repository ${HELM_REPO_NAME} already exists - updating repository instead."
-    fi
+  for repo in "${repos[@]}"; do
+    key="${repo%%=*}"
+    value="${repo##*=}"
 
-    helm repo update
+    if ! helm repo list | grep -q "^$key"; then
+      helm repo add "$key" "$value"
+    else
+      echo "Repository $key already exists - updating repository instead."
+    fi
+  done
+
+  helm repo update
 }
 
-# install the latest ibmcloud cli on Linux
 install_ibmcloud() {
   if [[ -x "$(command -v ibmcloud)" ]]; then
     echo "ibmcloud is already installed."
@@ -82,129 +70,136 @@ install_helm() {
   fi
 }
 
-LOGFILE="pr-${GIT_PR_NUMBER}-openshift-tests-${BUILD_NUMBER}"
-echo "Log file: ${LOGFILE}"
-TEST_NAME="backstage-showcase Tests"
-
-source ./.ibm/pipelines/functions.sh
-
-skip_if_only
-
-# install ibmcloud
-install_ibmcloud
-
-ibmcloud version
-ibmcloud config --check-version=false
-ibmcloud plugin install -f container-registry
-ibmcloud plugin install -f kubernetes-service
-
-# Using pipeline configuration - environment properties
-ibmcloud login -r "${IBM_REGION}" -g "${IBM_RSC_GROUP}" --apikey "${SERVICE_ID_API_KEY}"
-ibmcloud oc cluster config --cluster "${OPENSHIFT_CLUSTER_ID}"
-
-install_oc
-
-oc version --client
-# oc login -u apikey -p "${SERVICE_ID_API_KEY}" --server="${IBM_OPENSHIFT_ENDPOINT}"
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "$DIR"
-
-oc login --token=${K8S_CLUSTER_TOKEN} --server=${K8S_CLUSTER_URL}
-
-# refresh the name space if exists
-if oc get namespace ${NAME_SPACE} > /dev/null 2>&1; then
+configure_namespace() {
+  if oc get namespace ${NAME_SPACE} >/dev/null 2>&1; then
     echo "Namespace ${NAME_SPACE} already exists! refreshing namespace"
     oc delete namespace ${NAME_SPACE}
-fi
-oc create namespace ${NAME_SPACE}
+  fi
+  oc create namespace ${NAME_SPACE}
+  oc config set-context --current --namespace=${NAME_SPACE}
+}
 
-oc config set-context --current --namespace=${NAME_SPACE}
+apply_yaml_files() {
+  local dir=$1
 
-install_helm
+  # Update namespace and other configurations in YAML files
+  local files=("$dir/resources/service_account/service-account-rhdh.yaml"
+    "$dir/resources/cluster_role_binding/cluster-role-binding-k8s.yaml"
+    "$dir/resources/cluster_role_binding/cluster-role-binding-ocm.yaml")
+  
+  for file in "${files[@]}"; do
+    sed -i "s/namespace:.*/namespace: $NAME_SPACE/g" "$file"
+  done
 
-cd $DIR
+  # Add additional configurations
+  sed -i "s/backstage.io\/kubernetes-id:.*/backstage.io\/kubernetes-id: $K8S_PLUGIN_ANNOTATION/g" "$dir/resources/deployment/deployment-test-app-component.yaml"
 
-# Change the namespace of the resources to the one namespace set above
-sed -i "s/namespace:.*/namespace: $NAME_SPACE/g" $DIR/resources/service_account/service-account-rhdh.yaml
-sed -i "s/namespace:.*/namespace: $NAME_SPACE/g" $DIR/resources/cluster_role_binding/cluster-role-binding-k8s.yaml
-sed -i "s/namespace:.*/namespace: $NAME_SPACE/g" $DIR/resources/cluster_role_binding/cluster-role-binding-ocm.yaml
+  for key in GITHUB_APP_APP_ID GITHUB_APP_CLIENT_ID GITHUB_APP_PRIVATE_KEY GITHUB_APP_CLIENT_SECRET GITHUB_APP_WEBHOOK_URL GITHUB_APP_WEBHOOK_SECRET KEYCLOAK_URL KEYCLOAK_REALM KEYCLOAK_CLIENT_SECRET KEYCLOAK_CLIENT_ID; do
+    sed -i "s/$key:.*/$key: ${!key}/g" "$dir/auth/secrets-rhdh-secrets.yaml"
+  done
 
-sed -i "s/backstage.io\/kubernetes-id:.*/backstage.io\/kubernetes-id: $K8S_PLUGIN_ANNOTATION/g" $DIR/resources/deployment/deployment-test-app-component.yaml
+  oc apply -f $dir/resources/service_account/service-account-rhdh.yaml --namespace=${NAME_SPACE}
+  oc apply -f $dir/auth/service-account-rhdh-secret.yaml --namespace=${NAME_SPACE}
+  oc apply -f $dir/auth/secrets-rhdh-secrets.yaml --namespace=${NAME_SPACE}
+  oc apply -f $dir/resources/deployment/deployment-test-app-component.yaml --namespace=${NAME_SPACE}
+  oc new-app https://github.com/janus-qe/test-backstage-customization-provider --namespace=${NAME_SPACE}
+  oc expose svc/test-backstage-customization-provider --namespace=${NAME_SPACE}
+  oc apply -f $dir/resources/cluster_role/cluster-role-k8s.yaml
+  oc apply -f $dir/resources/cluster_role_binding/cluster-role-binding-k8s.yaml
+  oc apply -f $dir/resources/cluster_role/cluster-role-ocm.yaml
+  oc apply -f $dir/resources/cluster_role_binding/cluster-role-binding-ocm.yaml
 
-sed -i "s/GITHUB_APP_APP_ID:.*/GITHUB_APP_APP_ID: $GITHUB_APP_APP_ID/g" $DIR/auth/secrets-rhdh-secrets.yaml
-sed -i "s/GITHUB_APP_CLIENT_ID:.*/GITHUB_APP_CLIENT_ID: $GITHUB_APP_CLIENT_ID/g" $DIR/auth/secrets-rhdh-secrets.yaml
-sed -i "s/GITHUB_APP_PRIVATE_KEY:.*/GITHUB_APP_PRIVATE_KEY: $GITHUB_APP_PRIVATE_KEY/g" $DIR/auth/secrets-rhdh-secrets.yaml
-sed -i "s/GITHUB_APP_CLIENT_SECRET:.*/GITHUB_APP_CLIENT_SECRET: $GITHUB_APP_CLIENT_SECRET/g" $DIR/auth/secrets-rhdh-secrets.yaml
-sed -i "s/GITHUB_APP_WEBHOOK_URL:.*/GITHUB_APP_WEBHOOK_URL: $GITHUB_APP_WEBHOOK_URL/g" $DIR/auth/secrets-rhdh-secrets.yaml
-sed -i "s/GITHUB_APP_WEBHOOK_SECRET:.*/GITHUB_APP_WEBHOOK_SECRET: $GITHUB_APP_WEBHOOK_SECRET/g" $DIR/auth/secrets-rhdh-secrets.yaml
+  # obtain K8S_CLUSTER_NAME, K8S_CLUSTER_API_SERVER_URL and add them to secrets-rhdh-secrets.yaml
+  # K8S_SERVICE_ACCOUNT_TOKEN will be replaced
+  oc get secret rhdh-k8s-plugin-secret -o yaml >$dir/auth/service-account-rhdh-token.yaml
 
-oc apply -f $DIR/resources/service_account/service-account-rhdh.yaml --namespace=${NAME_SPACE}
-oc apply -f $DIR/auth/service-account-rhdh-secret.yaml --namespace=${NAME_SPACE}
-oc apply -f $DIR/auth/secrets-rhdh-secrets.yaml --namespace=${NAME_SPACE}
+  TOKEN=$(grep 'token:' $dir/auth/service-account-rhdh-token.yaml | awk '{print $2}')
 
-oc apply -f $DIR/resources/deployment/deployment-test-app-component.yaml --namespace=${NAME_SPACE}
+  sed -i "s/K8S_SERVICE_ACCOUNT_TOKEN:.*/K8S_SERVICE_ACCOUNT_TOKEN: $TOKEN/g" $dir/auth/secrets-rhdh-secrets.yaml
 
-oc new-app https://github.com/janus-qe/test-backstage-customization-provider --namespace=${NAME_SPACE}
-oc expose svc/test-backstage-customization-provider --namespace=${NAME_SPACE}
+  # Cleanup temp file
+  rm $dir/auth/service-account-rhdh-token.yaml
 
-oc apply -f $DIR/resources/cluster_role/cluster-role-k8s.yaml 
-oc apply -f $DIR/resources/cluster_role_binding/cluster-role-binding-k8s.yaml 
-oc apply -f $DIR/resources/cluster_role/cluster-role-ocm.yaml
-oc apply -f $DIR/resources/cluster_role_binding/cluster-role-binding-ocm.yaml
+  # oc apply -f $dir/auth/rhdh-quay-pull-secret.yaml --namespace=${NAME_SPACE}
 
-# obtain K8S_CLUSTER_NAME, K8S_CLUSTER_API_SERVER_URL and add them to secrets-rhdh-secrets.yaml
-# K8S_SERVICE_ACCOUNT_TOKEN will be replaced
-oc get secret rhdh-k8s-plugin-secret -o yaml > $DIR/auth/service-account-rhdh-token.yaml
+  # re-apply with the updated cluster service account token
+  oc apply -f $dir/auth/secrets-rhdh-secrets.yaml --namespace=${NAME_SPACE}
+  oc apply -f $dir/resources/config_map/configmap-app-config-rhdh.yaml --namespace=${NAME_SPACE}
+}
 
-TOKEN=$(grep 'token:' $DIR/auth/service-account-rhdh-token.yaml | awk '{print $2}')
+run_tests() {
+  cd $DIR/../../e2e-test
+  yarn install
 
-sed -i "s/K8S_SERVICE_ACCOUNT_TOKEN:.*/K8S_SERVICE_ACCOUNT_TOKEN: $TOKEN/g" $DIR/auth/secrets-rhdh-secrets.yaml
+  Xvfb :99 &
+  export DISPLAY=:99
 
-# Cleanup temp file
-rm $DIR/auth/service-account-rhdh-token.yaml
-
-# oc apply -f $DIR/auth/rhdh-quay-pull-secret.yaml --namespace=${NAME_SPACE}
-
-# re-apply with the updated cluster service account token
-oc apply -f $DIR/auth/secrets-rhdh-secrets.yaml --namespace=${NAME_SPACE}
-oc apply -f $DIR/resources/config_map/configmap-app-config-rhdh.yaml --namespace=${NAME_SPACE}
-
-add_helm_repos
-
-GIT_PR_RESPONSE=$(curl -s "https://api.github.com/repos/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}/pulls/${GIT_PR_NUMBER}")
-LONG_SHA=$(echo "$GIT_PR_RESPONSE" | jq -r '.head.sha')
-SHORT_SHA=$(git rev-parse --short ${LONG_SHA})
-
-echo "Tag name with short SHA: pr-${GIT_PR_NUMBER}-${SHORT_SHA}"
-
-helm upgrade -i ${RELEASE_NAME} -n ${NAME_SPACE} ${HELM_REPO_NAME}/${HELM_IMAGE_NAME} -f $DIR/value_files/${HELM_CHART_VALUE_FILE_NAME} --set global.clusterRouterBase=${K8S_CLUSTER_ROUTER_BASE} --set upstream.backstage.image.tag=pr-${GIT_PR_NUMBER}-${SHORT_SHA}
-
-echo "Waiting for backstage deployment..."
-sleep 120
-
-echo "Display pods for verification..."
-oc get pods -n ${NAME_SPACE}
-
-# Check if Backstage is up and running
-BACKSTAGE_URL_RESPONSE=$(curl -Is "https://${RELEASE_NAME}-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}" | head -n 1)
-echo "$BACKSTAGE_URL_RESPONSE"
-
-cd $DIR/../../e2e-test
-yarn install
-
-Xvfb :99 &
-export DISPLAY=:99
-
-(
+  (
     set -e
-    echo Using PR container image: pr-${GIT_PR_NUMBER}
+    echo Using PR container image: pr-${GIT_PR_NUMBER}-${SHORT_SHA}
     yarn run cypress:run --config baseUrl="https://${RELEASE_NAME}-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}"
-) |& tee "/tmp/${LOGFILE}"
+  ) |& tee "/tmp/${LOGFILE}"
 
-RESULT=${PIPESTATUS[0]}
+  RESULT=${PIPESTATUS[0]}
 
-pkill Xvfb
+  pkill Xvfb
 
-save_logs "${LOGFILE}" "${TEST_NAME}" ${RESULT}
+  save_logs "${LOGFILE}" "${TEST_NAME}" ${RESULT}
 
-exit ${RESULT}
+  exit ${RESULT}
+}
+
+check_backstage_running() {
+  # Check if Backstage is up and running
+  BACKSTAGE_URL_RESPONSE=$(curl -Is "https://${RELEASE_NAME}-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}" | head -n 1)
+  echo "$BACKSTAGE_URL_RESPONSE"
+}
+
+main() {
+  echo "Log file: ${LOGFILE}"
+
+  source ./.ibm/pipelines/functions.sh
+  skip_if_only
+
+  DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+  install_ibmcloud
+  ibmcloud version
+  ibmcloud config --check-version=false
+  ibmcloud plugin install -f container-registry
+  ibmcloud plugin install -f kubernetes-service
+  ibmcloud login -r "${IBM_REGION}" -g "${IBM_RSC_GROUP}" --apikey "${SERVICE_ID_API_KEY}"
+  ibmcloud oc cluster config --cluster "${OPENSHIFT_CLUSTER_ID}"
+
+  install_oc
+  oc version --client
+  oc login --token=${K8S_CLUSTER_TOKEN} --server=${K8S_CLUSTER_URL}
+
+  configure_namespace
+  install_helm
+
+  cd $DIR
+  apply_yaml_files $DIR
+
+  add_helm_repos
+
+  GIT_PR_RESPONSE=$(curl -s "https://api.github.com/repos/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}/pulls/${GIT_PR_NUMBER}")
+  LONG_SHA=$(echo "$GIT_PR_RESPONSE" | jq -r '.head.sha')
+  SHORT_SHA=$(git rev-parse --short ${LONG_SHA})
+
+  echo "Tag name with short SHA: pr-${GIT_PR_NUMBER}-${SHORT_SHA}"
+
+  helm upgrade -i ${RELEASE_NAME} -n ${NAME_SPACE} ${HELM_REPO_NAME}/${HELM_IMAGE_NAME} --version ${CHART_VERSION} -f $DIR/value_files/${HELM_CHART_VALUE_FILE_NAME} --set global.clusterRouterBase=${K8S_CLUSTER_ROUTER_BASE} --set upstream.backstage.image.tag=pr-${GIT_PR_NUMBER}-${SHORT_SHA}
+
+  echo "Waiting for backstage deployment..."
+  sleep 500
+
+  echo "Display pods for verification..."
+  oc get pods -n ${NAME_SPACE}
+
+  check_backstage_running
+
+  run_tests
+}
+
+main
--- e2e-test/cypress/e2e/guest-signin-happy-path.spec.ts
+++ e2e-test/cypress/e2e/guest-signin-happy-path.spec.ts
@@ -1,7 +1,6 @@
 import { UIhelper } from '../utils/UIhelper';
 import { HomePage } from '../support/pages/HomePage';
 import { Common } from '../utils/Common';
-import { gpts } from '../support/testData/gpts';
 
 describe('Test with Guest Sign-in', () => {
   before(() => {
@@ -10,7 +9,6 @@ describe('Test with Guest Sign-in', () => {
 
   it('Verify the Homepage renders with Search Bar, Quick Access and Starred Entities', () => {
     UIhelper.verifyHeading('Welcome back!');
-    // HomePage.verifyQuickSearchBar("subhashkhileri");
     UIhelper.openSidebar('Home');
     HomePage.verifyQuickAccess('Developer Tools', 'Podman Desktop');
   });
@@ -19,31 +17,5 @@ describe('Test with Guest Sign-in', () => {
     UIhelper.openSidebar('Catalog');
     UIhelper.verifyHeading('My Org Catalog');
     UIhelper.selectMuiBox('Kind', 'Component');
-    // UIhelper.verifyRowsInTable(['No records to display'])
-  });
-
-  it('Verify that all users in your Github Organization have been ingested into the Catalog Page', () => {
-    UIhelper.selectMuiBox('Kind', 'User');
-    UIhelper.verifyRowsInTable(['subhashkhileri', 'josephca', 'gustavolira']);
-  });
-
-  it('Verify all 12 GPTs appear in the Create page', () => {
-    UIhelper.openSidebar('Create...');
-    UIhelper.verifyHeading('Golden Path Templates');
-
-    gpts.forEach(gpt => {
-      UIhelper.verifyHeading(gpt);
-    });
-  });
-
-  it("Verify Profile is Guest in the Settings page (Ensure backstage identity's User Entity is user:default/guest)", () => {
-    UIhelper.openSidebar('Settings');
-    UIhelper.verifyHeading('Guest');
-    UIhelper.verifyHeading('User Entity: user:default/guest');
-  });
-
-  it('Sign Out and Verify that you return to the Sign-in page', () => {
-    UIhelper.openSidebar('Settings');
-    Common.signOut();
   });
 });
