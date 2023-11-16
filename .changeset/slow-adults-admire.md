34c99ab00865a8f5f1f96da9a614dadc9d71712b
Tom Coufal <7453394+tumido@users.noreply.github.com>
2023-11-16 21:15:42 +0100

    feat(dynamic-plugins): add techdocs dynamic wrapper (#775)
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>
    Co-authored-by: Nick Boldt <nboldt@redhat.com>
    Co-authored-by: David Festal <dfestal@redhat.com>

---
+++ .changeset/slow-adults-admire.md
@@ -0,0 +1,6 @@
+---
+'backstage-plugin-techdocs': minor
+'app': minor
+---
+
+Change TechDocs frontend to a dynamic plugin and allow search page extensions through mountpoints
--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -20,9 +20,6 @@ describe('App', () => {
           data: {
             app: { title: 'Test' },
             backend: { baseUrl: 'http://localhost:7007' },
-            techdocs: {
-              storageUrl: 'http://localhost:7007/api/techdocs/static/docs',
-            },
             auth: { environment: 'development' },
           },
           context: 'test',
