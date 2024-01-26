b08f5ea69866d836342077f005b1814ded752df5
David Festal <dfestal@redhat.com>
2024-01-26 11:49:14 +0100

    Update wrappers
    Signed-off-by: David Festal <dfestal@redhat.com>

--- dynamic-plugins/wrappers/backstage-plugin-catalog-backend-module-bitbucket-cloud/src/index.ts
+++ dynamic-plugins/wrappers/backstage-plugin-catalog-backend-module-bitbucket-cloud/src/index.ts
@@ -1,1 +1,1 @@
-export * from './dynamic/index';
+export { default } from '@backstage/plugin-catalog-backend-module-bitbucket-cloud/alpha';
--- dynamic-plugins/wrappers/backstage-plugin-catalog-backend-module-bitbucket-server/src/dynamic/alpha.ts
+++
@@ -1,7 +0,0 @@
-import { BackendDynamicPluginInstaller } from '@backstage/backend-dynamic-feature-service';
-import catalogModuleBitbucketServerEntityProvider from '@backstage/plugin-catalog-backend-module-bitbucket-server/alpha';
-
-export const dynamicPluginInstaller: BackendDynamicPluginInstaller = {
-  kind: 'new',
-  install: () => [catalogModuleBitbucketServerEntityProvider()],
-};
