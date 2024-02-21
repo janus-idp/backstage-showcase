37fc4760e653c85f86ae3a4aa830000f8c2a1db2
Yi Cai <yicai@redhat.com>
2024-02-21 04:32:49 -0500

    fix(ui): add pf5 button style config (#957)
    * fix(ui): add pf5 button style config
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    * Added changeset
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    * Updated contained button background color in dark theme
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    * Minor typo fix
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    * Fixed minor typo
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    * Code clean up
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    * Addressed comments
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    ---------
    
    Signed-off-by: Yi Cai <yicai@redhat.com>

--- packages/app/src/themes/componentOverrides.ts
+++ packages/app/src/themes/componentOverrides.ts
@@ -1,4 +1,5 @@
 import { UnifiedThemeOptions } from '@backstage/theme';
+import { defaultThemePalette } from './defaultThemePalette';
 import { ThemeColors } from '../types/types';
 
 const redhatFont = `@font-face {
@@ -13,7 +14,9 @@ const redhatFont = `@font-face {
 
 export const components = (
   themeColors: ThemeColors,
+  mode: string,
 ): UnifiedThemeOptions['components'] => {
+  const themePalette = defaultThemePalette(mode);
   return {
     BackstageHeaderTabs: {
       styleOverrides: {
@@ -54,6 +57,139 @@ export const components = (
         },
         disabled: {
           backgroundColor: '#6a6e73',
+        },
+      },
+    },
+    MuiButton: {
+      defaultProps: {
+        disableRipple: true,
+      },
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+          border: '0',
+          borderRadius: '3px',
+        },
+        contained: {
+          boxShadow: 'none',
+          '&:hover': {
+            border: '0',
+            boxShadow: 'none',
+          },
+          '&:-webkit-any-link:focus-visible': {
+            outlineOffset: '0',
+          },
+        },
+        containedPrimary: {
+          backgroundColor: themePalette.primary.containedButtonBackground,
+          color: themePalette.primary.contrastText,
+          '&:hover': {
+            backgroundColor: themePalette.primary.dark,
+            color: themePalette.primary.contrastText,
+          },
+          '&:focus-visible': {
+            boxShadow: `inset 0 0 0 1px ${themePalette.primary.focusVisibleBorder}`,
+            outline: `${themePalette.primary.focusVisibleBorder} solid 1px`,
+            backgroundColor: themePalette.primary.dark,
+            color: themePalette.primary.contrastText,
+          },
+          '&:disabled': {
+            color: themePalette.primary.disabled,
+            backgroundColor: themePalette.primary.disabledBackground,
+          },
+        },
+        containedSecondary: {
+          backgroundColor: themePalette.secondary.containedButtonBackground,
+          color: themePalette.secondary.contrastText,
+          '&:hover': {
+            backgroundColor: themePalette.secondary.dark,
+            color: themePalette.secondary.contrastText,
+          },
+          '&:focus-visible': {
+            boxShadow: `inset 0 0 0 1px ${themePalette.secondary.focusVisibleBorder}`,
+            outline: `${themePalette.secondary.focusVisibleBorder} solid 1px`,
+            backgroundColor: themePalette.secondary.dark,
+            color: themePalette.secondary.contrastText,
+          },
+          '&:disabled': {
+            color: themePalette.secondary.disabled,
+            backgroundColor: themePalette.secondary.disabledBackground,
+          },
+        },
+        outlined: {
+          border: '0',
+          boxShadow: `inset 0 0 0 1px ${themePalette.primary.main}`,
+          '&:hover': {
+            border: '0',
+            boxShadow: `inset 0 0 0 2px ${themePalette.primary.main}`,
+            backgroundColor: 'transparent',
+          },
+          '&:focus-visible': {
+            boxShadow: `inset 0 0 0 2px ${themePalette.primary.main}`,
+            outline: `${themePalette.primary.focusVisibleBorder} solid 1px`,
+          },
+        },
+        outlinedPrimary: {
+          color: themePalette.primary.main,
+          boxShadow: `inset 0 0 0 1px ${themePalette.primary.main}`,
+          border: '0',
+          '&:hover': {
+            border: '0',
+            boxShadow: `inset 0 0 0 2px ${themePalette.primary.main}`,
+            backgroundColor: 'transparent',
+          },
+          '&:focus-visible': {
+            boxShadow: `inset 0 0 0 2px ${themePalette.primary.main}`,
+            outline: `${themePalette.primary.focusVisibleBorder} solid 1px`,
+          },
+        },
+        outlinedSecondary: {
+          color: themePalette.secondary.main,
+          boxShadow: `inset 0 0 0 1px ${themePalette.secondary.main}`,
+          border: '0',
+          '&:hover': {
+            border: '0',
+            boxShadow: `inset 0 0 0 2px ${themePalette.secondary.main}`,
+            backgroundColor: 'transparent',
+          },
+          '&:focus-visible': {
+            boxShadow: `inset 0 0 0 2px ${themePalette.secondary.main}`,
+            outline: `${themePalette.secondary.focusVisibleBorder} solid 1px`,
+          },
+        },
+        text: {
+          color: themePalette.primary.main,
+          '&:hover': {
+            color: themePalette.primary.textHover,
+            backgroundColor: 'transparent',
+          },
+          '&:focus-visible': {
+            boxShadow: `inset 0 0 0 2px ${themePalette.primary.main}`,
+            outline: `${themePalette.primary.focusVisibleBorder} solid 1px`,
+          },
+        },
+        textPrimary: {
+          color: themePalette.primary.main,
+          '&:hover': {
+            color: themePalette.primary.textHover,
+            backgroundColor: 'transparent',
+          },
+        },
+        textSecondary: {
+          color: themePalette.secondary.main,
+          '&:hover': {
+            color: themePalette.secondary.textHover,
+            backgroundColor: 'transparent',
+          },
+        },
+      },
+    },
+    MuiLink: {
+      styleOverrides: {
+        underlineHover: {
+          '&:hover': {
+            textDecoration: 'none',
+          },
         },
       },
     },
