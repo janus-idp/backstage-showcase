bfbc3db6782f39b26eccc308589e22f27accf069
Tomas Kral <tkral@redhat.com>
2024-02-21 21:01:29 +0100

    feat: enable techdocs by default (#973)
    * feat: enable techdocs by default
    
    * chore: add e2e test for TechDocs
    
    * chore(ci): don't override defualt techdocs config

---
+++ e2e-tests/playwright/e2e/techdocs.spec.ts
@@ -0,0 +1,25 @@
+import { test } from '@playwright/test';
+import { UIhelper } from '../utils/UIhelper';
+import { Common } from '../utils/Common';
+
+test.describe.serial('TechDocs', () => {
+  let common: Common;
+  let uiHelper: UIhelper;
+
+  test.beforeEach(async ({ page }) => {
+    uiHelper = new UIhelper(page);
+    common = new Common(page);
+    await common.loginAsGuest();
+  });
+
+  test('Verify that TechDocs is visible in sidebar', async () => {
+    await uiHelper.openSidebar('Docs');
+  });
+
+  test('Verify that TechDocs for Backstage Showcase works', async ({
+    page,
+  }) => {
+    await uiHelper.openSidebar('Docs');
+    await page.getByRole('link', { name: 'Backstage Showcase' }).click();
+  });
+});
