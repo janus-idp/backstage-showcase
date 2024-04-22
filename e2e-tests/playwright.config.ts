ee0baae337d5579a61707ee1916d20d56f69dfd3
Zbyněk Drápela <zbynek.drapela@gmail.com>
2024-04-22 15:02:28 +0200

    RHIDP-428 Add JUnit test results reporter

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'], ['junit', { outputFile: 'junit-results.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: process.env.BASE_URL,
