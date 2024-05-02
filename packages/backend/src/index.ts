4a68eb4a7aec9e8d11b0f0b044fd385e9476a5a6
Armel Soro <asoro@redhat.com>
2024-05-02 16:05:25 +0200

    Add proxy agent to allow backend requests to support corporate proxies [1]
    [1] https://github.com/backstage/backstage/blob/master/contrib/docs/tutorials/help-im-behind-a-corporate-proxy.md#installation

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -17,6 +17,27 @@ import {
   pluginIDProviderService,
   rbacDynamicPluginsProvider,
 } from './modules/rbacDynamicPluginsModule';
+import 'global-agent/bootstrap';
+import { setGlobalDispatcher, ProxyAgent } from 'undici';
+
+const proxyEnv =
+  process.env.GLOBAL_AGENT_HTTP_PROXY ||
+  process.env.GLOBAL_AGENT_HTTPS_PROXY;
+
+if (proxyEnv) {
+  const proxyUrl = new URL(proxyEnv);
+  setGlobalDispatcher(
+    new ProxyAgent({
+      uri: proxyUrl.protocol + proxyUrl.host,
+      token:
+        proxyUrl.username && proxyUrl.password
+          ? `Basic ${Buffer.from(
+              `${proxyUrl.username}:${proxyUrl.password}`,
+            ).toString('base64')}`
+          : undefined,
+    }),
+  );
+}
 
 const backend = createBackend();
 
