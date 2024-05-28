16b888905c26805d9647b87ea582a929d67671e4
abarbaro <abarbaro@redhat.com>
2024-05-28 16:55:20 +0200

    add attachemnt to report

--- e2e-tests/playwright/e2e/custom-theme.spec.ts
+++ e2e-tests/playwright/e2e/custom-theme.spec.ts
@@ -28,25 +28,23 @@ test.describe('CustomTheme should be applied', () => {
       'none, linear-gradient(90deg, rgb(248, 248, 248), rgb(248, 248, 248))',
     );
 
-    const screenshot_1 = await page.screenshot({
+    await page.screenshot({
       path: 'screenshots/cusotm-theme-inspection.png',
       fullPage: true,
     });
 
     await testInfo.attach('screenshot', {
-      body: screenshot_1,
-      contentType: 'image/png',
+      path: 'screenshots/cusotm-theme-inspection.png',
     });
 
     await page.locator('[name=pin]').click();
 
-    const screenshot_2 = await page.screenshot({
+    await page.screenshot({
       path: 'screenshots/cusotm-theme-inspection-collapsed.png',
       fullPage: true,
     });
     await testInfo.attach('screenshot', {
-      body: screenshot_2,
-      contentType: 'image/png',
+      path: 'screenshots/cusotm-theme-inspection-collapsed.png',
     });
 
     await common.signOut();
