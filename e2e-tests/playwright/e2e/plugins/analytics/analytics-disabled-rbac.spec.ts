2912403747fa096c136caf3dd7edeae13bb5472e
Zbyněk Drápela <zbynek.drapela@gmail.com>
2024-06-04 13:01:23 +0200

    Verify plugin disabled RBAC in UI

--- e2e-tests/playwright/e2e/plugins/analytics/analytics-disabled-rbac.spec.ts
+++ e2e-tests/playwright/e2e/plugins/analytics/analytics-disabled-rbac.spec.ts
@@ -1,34 +1,33 @@
-import { test, expect, Page } from '@playwright/test';
-import { Analytics } from '../../../utils/analytics/analytics';
-import { Common, setupBrowser } from '../../../utils/Common';
+import { test, expect } from '@playwright/test';
+import { Common } from '../../../utils/Common';
 import { UIhelper } from '../../../utils/UIhelper';
+import { UIhelperPO } from '../../../support/pageObjects/global-obj';
 
 test.describe('Check RBAC "analytics-provider-segment" plugin', () => {
   let common: Common;
   let uiHelper: UIhelper;
-  let page: Page;
 
-  test.beforeAll(async ({ browser }, testInfo) => {
-    page = (await setupBrowser(browser, testInfo)).page;
-
+  test.beforeAll(async ({ page }) => {
     uiHelper = new UIhelper(page);
     common = new Common(page);
     await common.loginAsGithubUser();
     await uiHelper.openSidebar('Administration');
     await uiHelper.verifyHeading('Administration');
-    await uiHelper.verifyLink('RBAC');
-    await uiHelper.clickTab('RBAC');
+    await uiHelper.verifyLink('Plugins');
+    await uiHelper.clickTab('Plugins');
   });
 
-  test('is disabled', async () => {
-    const analytics = new Analytics();
-    const authHeader = await analytics.getAuthHeader();
-    const pluginsList = await analytics.getDynamicPluginsList(authHeader);
-    const isPluginListed = analytics.checkPluginListed(
-      pluginsList,
-      '@janus-idp/backstage-plugin-analytics-provider-segment',
+  test('is disabled', async ({ page }) => {
+    await page
+      .getByPlaceholder('Filter')
+      .pressSequentially('backstage-plugin-analytics-provider-segment\n', {
+        delay: 300,
+      });
+    const row = await page.locator(
+      UIhelperPO.rowByText(
+        '@janus-idp/backstage-plugin-analytics-provider-segment',
+      ),
     );
-
-    expect(isPluginListed).toBe(false);
+    expect(await row.locator('td').nth(2).innerText()).toBe('No'); // not enabled
   });
 });
