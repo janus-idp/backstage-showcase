07bb7900f09f12738d63cf481a8d7bd3f00f3dfb
Sanket <ssaikia@redhat.com>
2024-06-12 14:47:33 +0530

    [Test automation] OpenShift Learning Path. The link from the learning path should open a new tab

---
+++ e2e-tests/playwright/e2e/learning-path-page.spec.ts
@@ -0,0 +1,29 @@
+import { test } from '@playwright/test';
+import { UIhelper } from '../utils/UIhelper';
+import { Common } from '../utils/Common';
+
+test.describe('Learning Paths', () => {
+  let common: Common;
+  let uiHelper: UIhelper;
+
+  test.beforeEach(async ({ page }) => {
+    uiHelper = new UIhelper(page);
+    common = new Common(page);
+    await common.loginAsGuest();
+  });
+
+  test('Verify that links in Learning Paths for Backstage opens in a new tab', async ({
+    page,
+  }) => {
+    await uiHelper.openSidebar('Learning Paths');
+
+    for (let i = 0; i < 5; i++) {
+      const popupPromise = page.waitForEvent('popup');
+      await page.locator(`div[class*="MuiCardHeader-root"]`).nth(i).click();
+      const popup = await popupPromise;
+      await popup.waitForLoadState();
+      console.log(await popup.evaluate('location.href'));
+      await popup.close();
+    }
+  });
+});
