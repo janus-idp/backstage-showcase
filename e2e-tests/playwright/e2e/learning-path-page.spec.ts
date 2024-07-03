7b52faf6b27152f6d207d0580d966da70e39c04f
Sanket <ssaikia@redhat.com>
2024-07-03 12:49:11 +0530

    Changing PR fix

--- e2e-tests/playwright/e2e/learning-path-page.spec.ts
+++ e2e-tests/playwright/e2e/learning-path-page.spec.ts
@@ -22,7 +22,7 @@ test.describe('Learning Paths', () => {
       const popupPromise = page.waitForEvent('popup');
       await page.locator(`div[class*="MuiCardHeader-root"]`).nth(i).click();
       const popup = await popupPromise;
-      await popup.waitForLoadState('domcontentloaded');
+      await popup.waitForLoadState('load', { timeout: 120000 });
       const url = await popup.evaluate('location.href');
       console.log(url);
       expect(url).not.toBe('about:blank');
