d9712d77288959c7833a9a83348b2619bf4f032b
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2024-07-19 13:24:50 +0200

    fix(ui): update config at runtime to apply branding (#1424)
    Signed-off-by: Yi Cai <yicai@redhat.com>
    Co-authored-by: Yi Cai <yicai@redhat.com>

---
+++ packages/app/src/components/Root/ConfigUpdater.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { configApiRef, useApi } from '@backstage/core-plugin-api';
+
+const ConfigUpdater: React.FC = () => {
+  const configApi = useApi(configApiRef);
+
+  useEffect(() => {
+    const updateConfig = () => {
+      const logoIconBase64URI = configApi.getOptionalString(
+        'app.branding.iconLogo',
+      );
+
+      if (logoIconBase64URI) {
+        const favicon = document.getElementById(
+          'dynamic-favicon',
+        ) as HTMLLinkElement;
+        if (favicon) {
+          favicon.href = logoIconBase64URI;
+        } else {
+          const newFavicon = document.createElement('link');
+          newFavicon.id = 'dynamic-favicon';
+          newFavicon.rel = 'icon';
+          newFavicon.href = logoIconBase64URI;
+          document.head.appendChild(newFavicon);
+        }
+      }
+    };
+
+    updateConfig();
+  }, [configApi]);
+
+  return null;
+};
+
+export default ConfigUpdater;
