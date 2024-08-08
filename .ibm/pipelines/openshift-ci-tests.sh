e5ff03b94d566263279ba41e97ba66b50c3cf5bc
Zbyněk Drápela <zbynek.drapela@gmail.com>
2024-08-08 09:36:43 +0200

    Add override to force run as AKS job

--- .ibm/pipelines/openshift-ci-tests.sh
+++ .ibm/pipelines/openshift-ci-tests.sh
@@ -9,6 +9,8 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 secret_name="rhdh-k8s-plugin-secret"
 OVERALL_RESULT=0
 
+JOB_NAME="periodic-aks"
+
 cleanup() {
   echo "Cleaning up before exiting"
   rm -rf ~/tmpbin
