b3b1f4519faca4e952360eec03b28e04783317cc
Subhash Khileri <skhileri@redhat.com>
2024-08-29 17:39:34 +0530

    save all pod logs

--- .ibm/pipelines/utils.sh
+++ .ibm/pipelines/utils.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
 retrieve_pod_logs() {
-  local POD_NAME=$1; local CONTAINER=$2; local NAMESPACE_NAME=$3
-  echo "  Retrieving logs for container: $CONTAINER"
+  local pod_name=$1; local container=$2; local namespace=$3
+  echo "  Retrieving logs for container: $container"
   # Save logs for the current and previous container
-  kubectl logs $POD_NAME -c $CONTAINER -n $NAMESPACE_NAME > "/tmp/pod_logs/${POD_NAME}_${CONTAINER}.log"
-  kubectl logs $POD_NAME -c $CONTAINER -n $NAMESPACE_NAME --previous > "/tmp/pod_logs/${POD_NAME}_${CONTAINER}-previous.log" 2>/dev/null || { echo "  Previous logs for container $CONTAINER not found"; rm -f "/tmp/pod_logs/${POD_NAME}_${CONTAINER}-previous.log"; }
+  kubectl logs $pod_name -c $container -n $namespace > "/tmp/pod_logs/${pod_name}_${container}.log"
+  kubectl logs $pod_name -c $container -n $namespace --previous > "/tmp/pod_logs/${pod_name}_${container}-previous.log" 2>/dev/null || { echo "  Previous logs for container $container not found"; rm -f "/tmp/pod_logs/${pod_name}_${container}-previous.log"; }
 }
 
 save_all_pod_logs(){
-  local NAMESPACE_NAME=$1
+  local namespace=$1
   # Get all pod names in the namespace
-  POD_NAMES=$(kubectl get pods -n $NAMESPACE_NAME -o jsonpath='{.items[*].metadata.name}')
+  pod_names=$(kubectl get pods -n $namespace -o jsonpath='{.items[*].metadata.name}')
 
-  for POD_NAME in $POD_NAMES; do
-    echo "Retrieving logs for pod: $POD_NAME in namespace $NAMESPACE_NAME"
+  for pod_name in $pod_names; do
+    echo "Retrieving logs for pod: $pod_name in namespace $namespace"
 
-    INIT_CONTAINERS=$(kubectl get pod $POD_NAME -n $NAMESPACE_NAME -o jsonpath='{.spec.initContainers[*].name}')
+    init_containers=$(kubectl get pod $pod_name -n $namespace -o jsonpath='{.spec.initContainers[*].name}')
     # Loop through each init container and retrieve logs
-    for INIT_CONTAINER in $INIT_CONTAINERS; do
-      retrieve_pod_logs $POD_NAME $INIT_CONTAINER $NAMESPACE_NAME
+    for init_container in $init_containers; do
+      retrieve_pod_logs $pod_name $init_container $namespace
     done
     
-    CONTAINERS=$(kubectl get pod $POD_NAME -n $NAMESPACE_NAME -o jsonpath='{.spec.containers[*].name}')
-    for CONTAINER in $CONTAINERS; do
-      retrieve_pod_logs $POD_NAME $CONTAINER $NAMESPACE_NAME
+    containers=$(kubectl get pod $pod_name -n $namespace -o jsonpath='{.spec.containers[*].name}')
+    for container in $containers; do
+      retrieve_pod_logs $pod_name $container $namespace
     done
   done
 
