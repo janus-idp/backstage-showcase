07e84a01e7bf753cc540661c1eb58e86c9bd990a
Subhash Khileri <skhileri@redhat.com>
2024-09-18 11:35:04 +0530

    chore(e2e): Bulk import e2e tests automation

--- e2e-tests/playwright/utils/APIEndpoints.ts
+++ e2e-tests/playwright/utils/APIEndpoints.ts
@@ -1,19 +1,31 @@
-const backstageShowcaseAPI =
-  'https://api.github.com/repos/janus-idp/backstage-showcase';
+const BASE_API_URL = 'https://api.github.com';
+const PER_PAGE = 100;
+
+const getRepoUrl = (owner: string, repo: string) =>
+  `${BASE_API_URL}/repos/${owner}/${repo}`;
+const getOrgUrl = (owner: string) => `${BASE_API_URL}/orgs/${owner}`;
+
+const backstageShowcaseAPI = getRepoUrl('janus-idp', 'backstage-showcase');
 
 export const githubAPIEndpoints = {
   pull: (owner: string, repo: string, state: 'open' | 'closed' | 'all') =>
-    `https://api.github.com/repos/${owner}/${repo}/pulls?per_page=100&state=${state}`,
+    `${getRepoUrl(owner, repo)}/pulls?per_page=${PER_PAGE}&state=${state}`,
+
   issues: (state: string) =>
-    `${backstageShowcaseAPI}/issues?per_page=100&sort=updated&state=${state}`,
-  workflowRuns: `${backstageShowcaseAPI}/actions/runs?per_page=100`,
-  deleteRepo: (owner: string, repo: string) =>
-    `https://api.github.com/repos/${owner}/${repo}`,
+    `${backstageShowcaseAPI}/issues?per_page=${PER_PAGE}&sort=updated&state=${state}`,
+
+  workflowRuns: `${backstageShowcaseAPI}/actions/runs?per_page=${PER_PAGE}`,
+
+  deleteRepo: getRepoUrl,
+
   mergePR: (owner: string, repoName: string, pull_number: number) =>
-    `https://api.github.com/repos/${owner}/${repoName}/pulls/${pull_number}/merge`,
-  createRepo: (owner: string) => `https://api.github.com/orgs/${owner}/repos`,
+    `${getRepoUrl(owner, repoName)}/pulls/${pull_number}/merge`,
+
+  createRepo: (owner: string) => `${getOrgUrl(owner)}/repos`,
+
   pull_files: (owner: string, repoName: string, pr: number) =>
-    `https://api.github.com/repos/${owner}/${repoName}/pulls/${pr}/files`,
+    `${getRepoUrl(owner, repoName)}/pulls/${pr}/files`,
+
   contents: (owner: string, repoName: string) =>
-    `https://api.github.com/repos/${owner}/${repoName}/contents`,
+    `${getRepoUrl(owner, repoName)}/contents`,
 };
