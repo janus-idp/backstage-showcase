c5034bc20ff2640b11bcb0b49577d80ea6903f05
Zbyněk Drápela <zbynek.drapela@gmail.com>
2024-09-19 09:30:57 +0200

    Disable broken analytics test

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -88,8 +88,9 @@ export default defineConfig({
       name: 'showcase-rbac-aks',
       ...useCommonDeviceAndViewportConfig,
       testMatch: [
+        '**/playwright/e2e/instance-health-check.spec.ts',
         // '**/playwright/e2e/plugins/rbac/**/*.spec.ts', // tests broken on AKS by https://github.com/janus-idp/backstage-showcase/pull/1565
-        '**/playwright/e2e/plugins/analytics/analytics-disabled-rbac.spec.ts',
+        // '**/playwright/e2e/plugins/analytics/analytics-disabled-rbac.spec.ts', // tests broken on AKS by https://github.com/janus-idp/backstage-showcase/pull/1565
       ],
     },
 
