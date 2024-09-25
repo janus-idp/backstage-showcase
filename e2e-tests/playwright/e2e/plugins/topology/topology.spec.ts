0cc7b0a39d98fbcf803acd6ce4298b1c66251c04
ssaikia <sanketsaikia13@gmail.com>
2024-09-25 10:21:38 +0530

    [Test automation] Verify Topology is a supported plugin and E2E scenario

--- e2e-tests/playwright/e2e/plugins/topology/topology.spec.ts
+++ e2e-tests/playwright/e2e/plugins/topology/topology.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { Common } from '../../../utils/Common';
 import { UIhelper } from '../../../utils/UIhelper';
 import { Catalog } from '../../../support/pages/Catalog';
@@ -15,11 +15,34 @@ test.describe('Test Topology Plugin', () => {
     await common.loginAsGuest();
   });
 
-  test('Verify pods visibility in the Topology tab', async () => {
+  test('Verify pods visibility in the Topology tab', async ({ page }) => {
     await catalog.goToBackstageJanusProject();
     await uiHelper.clickTab('Topology');
     await uiHelper.verifyText('backstage-janus');
     await uiHelper.verifyText('rhdh');
     await uiHelper.verifyText('rhdh-rbac');
+    const url_button = page
+      .locator('g')
+      .filter({ hasText: /Open URL/ })
+      .getByRole('button')
+      .first();
+    expect(await url_button.getAttribute('href')).toContain(
+      'https://rhdh-backstage-showcase',
+    );
+    await page
+      .locator('g')
+      .filter({ hasText: /Open URL/ })
+      .locator('image')
+      .first()
+      .click();
+    await page.getByLabel('Pod').click();
+    await page.getByLabel('Pod').getByText('1', { exact: true }).click();
+    await uiHelper.clickTab('Details');
+    await uiHelper.isTextVisible('Status');
+    await uiHelper.isTextVisible('Active');
+    await uiHelper.clickTab('Resources');
+    await uiHelper.verifyHeading('Pods');
+    await uiHelper.verifyHeading('Services');
+    await uiHelper.verifyHeading('Routes');
   });
 });
