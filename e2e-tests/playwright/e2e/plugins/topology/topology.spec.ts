1e9e04a7af77810286dbfb75f423d1ef29e3049f
ssaikia <sanketsaikia13@gmail.com>
2024-09-25 10:21:38 +0530

    [Test automation] Verify Topology is a supported plugin and E2E scenario

--- e2e-tests/playwright/e2e/plugins/topology/topology.spec.ts
+++ e2e-tests/playwright/e2e/plugins/topology/topology.spec.ts
@@ -1,7 +1,7 @@
-import { test } from "@playwright/test";
-import { Common } from "../../../utils/common";
-import { UIhelper } from "../../../utils/ui-helper";
-import { Catalog } from "../../../support/pages/catalog";
+import { test, expect } from "@playwright/test";
+import { Common } from "../../../utils/Common";
+import { UIhelper } from "../../../utils/UIhelper";
+import { Catalog } from "../../../support/pages/Catalog";
 
 test.describe("Test Topology Plugin", () => {
   let common: Common;
@@ -15,11 +15,43 @@ test.describe("Test Topology Plugin", () => {
     await common.loginAsGuest();
   });
 
-  test("Verify pods visibility in the Topology tab", async () => {
+  test("Verify pods visibility in the Topology tab", async ({ page }) => {
+    test.setTimeout(40000);
     await catalog.goToBackstageJanusProject();
     await uiHelper.clickTab("Topology");
     await uiHelper.verifyText("backstage-janus");
+    await page.getByRole("button", { name: "Fit to Screen" }).click();
     await uiHelper.verifyText("rhdh");
     await uiHelper.verifyText("rhdh-rbac");
+    await uiHelper.verifyButtonURL(
+      "Open URL",
+      "https://rhdh-backstage-showcase",
+    );
+    await page.locator("image").first().click();
+    await page.getByLabel("Pod").click();
+    await page.getByLabel("Pod").getByText("1", { exact: true }).click();
+    await uiHelper.clickTab("Details");
+    await uiHelper.verifyText("Status");
+    await uiHelper.verifyText("Active");
+    await uiHelper.clickTab("Resources");
+    await uiHelper.verifyHeading("Pods");
+    await uiHelper.verifyHeading("Services");
+    await uiHelper.verifyHeading("Routes");
+    await expect(
+      page.getByRole("link", { name: "https://rhdh-backstage-" }),
+    ).toBeVisible();
+    await expect(page.getByTitle("Deployment")).toBeVisible();
+    await uiHelper.verifyText("S");
+    await uiHelper.verifyText("RT");
+    await expect(
+      page.locator('[data-test-id="rhdh-rbac"] rect').first(),
+    ).toBeVisible();
+    await uiHelper.clickTab("Details");
+    await page.getByLabel("Pod").hover();
+    await page.getByLabel("Display options").click();
+    await page.getByLabel("Pod count").click();
+    await uiHelper.verifyText("1");
+    await uiHelper.verifyText("Pod");
+    await page.getByLabel("Pod count").click();
   });
 });
