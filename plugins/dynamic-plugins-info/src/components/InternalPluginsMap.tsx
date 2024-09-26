47f3fa1b8740049ab6ef41177cace208f3ac0953
Karthik Jeeyar <karthik.jk11@gmail.com>
2024-09-26 14:05:07 +0530

    fix(argocd): remove dist-dynamic folder (#1699)
    * fix(argocd): remove dist-dynamic folder
    
    * add argocd to the internalPluginsMap
    
    * add correct scalprum name

--- plugins/dynamic-plugins-info/src/components/InternalPluginsMap.tsx
+++ plugins/dynamic-plugins-info/src/components/InternalPluginsMap.tsx
@@ -77,8 +77,8 @@ export const InternalPluginsMap: Record<string, string> = {
     './dynamic-plugins/dist/janus-idp-backstage-plugin-3scale-backend-dynamic',
   '@janus-idp/backstage-plugin-keycloak-backend-dynamic':
     './dynamic-plugins/dist/janus-idp-backstage-plugin-keycloak-backend-dynamic',
-  '@janus-idp/backstage-plugin-argocd':
-    './dynamic-plugins/dist/janus-idp-backstage-plugin-argocd',
+  'backstage-community-plugin-redhat-argocd':
+    './dynamic-plugins/dist/backstage-community-plugin-redhat-argocd',
   'backstage-plugin-scaffolder-backend-module-bitbucket-cloud-dynamic':
     './dynamic-plugins/dist/backstage-plugin-scaffolder-backend-module-bitbucket-cloud-dynamic',
   'backstage-plugin-catalog-backend-module-bitbucket-cloud-dynamic':
