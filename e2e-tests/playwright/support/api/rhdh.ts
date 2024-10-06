b9c600497150a60bd73456964e71441192caf34c
Nil Gallego <github.i0l5z@simplelogin.com>
2024-10-06 20:29:28 +0200

    fix rhdh api

--- e2e-tests/playwright/support/api/rhdh.ts
+++ e2e-tests/playwright/support/api/rhdh.ts
@@ -1,6 +1,7 @@
 import { BrowserContext, request } from '@playwright/test';
 import { RhdhAuthHack } from './rhdh-auth-hack';
 import playwrightConfig from '../../../playwright.config';
+import { Role } from '../pages/rbac';
 
 export class RhdhApi {
   private readonly API_URL = `${playwrightConfig.use.baseURL}/api/`;
@@ -11,23 +12,25 @@ export class RhdhApi {
   }
 
   async getRoles(): Promise<any> {
-    const req = await this._permission().roles();
+    const req = await this._permission().roles().get();
     return req.json();
   }
 
   async getPolicies(): Promise<any> {
-    const req = await this._permission().policies();
+    const req = await this._permission().policies().get();
     return req.json();
   }
 
-  private async _myContext() {
+  private async _myContext(extraHeader?: { [key: string]: string }) {
     const auth = await RhdhAuthHack.getInstance().getApiToken(
       this.browserContext,
     );
+
     return request.newContext({
       baseURL: this.API_URL,
       extraHTTPHeaders: {
         authorization: auth,
+        ...extraHeader,
       },
     });
   }
@@ -35,13 +38,27 @@ export class RhdhApi {
   private _permission() {
     let url = `permission/`;
     return {
-      roles: async () => {
-        url += 'roles';
-        return (await this._myContext()).get(url);
+      roles: () => {
+        return {
+          get: async () => {
+            url += 'roles';
+            return (await this._myContext()).get(url);
+          },
+          post: async (role: Role) => {
+            url += 'roles';
+            return (
+              await this._myContext({ 'Content-Type': 'application/json' })
+            ).post(url, { data: role });
+          },
+        };
       },
-      policies: async () => {
-        url += 'policies';
-        return (await this._myContext()).get(url);
+      policies() {
+        return {
+          get: async () => {
+            url += 'policies';
+            return (await this._myContext()).get(url);
+          },
+        };
       },
     };
   }
