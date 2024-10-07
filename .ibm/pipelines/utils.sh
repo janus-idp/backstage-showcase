18de98f65cd0765010c2e0afb5cec6cf77769d58
Zbyněk Drápela <zbynek.drapela@gmail.com>
2024-10-07 12:52:53 +0200

    Change attachment folder

--- .ibm/pipelines/utils.sh
+++ .ibm/pipelines/utils.sh
@@ -92,6 +92,7 @@ droute_send() {
     --username '${DATA_ROUTER_USERNAME}' \
     --password '${DATA_ROUTER_PASSWORD}' \
     --results '/tmp/droute/${JUNIT_RESULTS}' \
+    --attachments '/tmp/droute/test-results' \
     --verbose"
 
 }
