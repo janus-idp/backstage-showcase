aa93b613643cb63688b49c8c65016d71c5e50191
Zbyněk Drápela <zbynek.drapela@gmail.com>
2024-10-08 16:27:13 +0200

    Test on AKS

--- .ibm/pipelines/openshift-ci-tests.sh
+++ .ibm/pipelines/openshift-ci-tests.sh
@@ -7,6 +7,7 @@ LOGFILE="test-log"
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 secret_name="rhdh-k8s-plugin-secret"
 OVERALL_RESULT=0
+JOB_NAME=periodic-aks
 
 cleanup() {
   echo "Cleaning up before exiting"
