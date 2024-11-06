51c2c386a9d8574cf4053ffd0814b626cfc7dbed
Nick Boldt <nboldt@redhat.com>
2024-11-06 16:23:54 -0500

    Update scripts/update-Dockerfile.sh

--- scripts/update-Dockerfile.sh
+++ scripts/update-Dockerfile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for dockerfile in ./docker/Dockerfile .rhdh/docker/Dockerfile; do
   sed -i "/# BEGIN COPY package.json files/,/# END COPY package.json files/c# BEGIN COPY package.json files\n# END COPY package.json files" $dockerfile
-  for path in $(find . -name package.json | grep -v node_modules/ ); do
+  for path in $(find . -name package.json | grep -v node_modules/ | sort -uV); do
     sed -i "s|\# BEGIN COPY package.json files|\# BEGIN COPY package.json files\nCOPY ${path/\./\$EXTERNAL_SOURCE_NESTED} $path|g" $dockerfile
   done
 done
