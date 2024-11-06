2e37fcfdac3ce36c4c2a8d53f5dfd40f61493a46
Nick Boldt <nboldt@redhat.com>
2024-11-06 15:57:00 -0500

    change both Dockerfiles; also include the changed content in this PR
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- scripts/update-Dockerfile.sh
+++ scripts/update-Dockerfile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-sed -i "/# BEGIN COPY package.json files/,/# END COPY package.json files/c# BEGIN COPY package.json files\n# END COPY package.json files" ./docker/Dockerfile
-for path in $(find . -name package.json | grep -v node_modules/ ); 
-  do sed -i "s|\# BEGIN COPY package.json files|\# BEGIN COPY package.json files\nCOPY $(echo $path | sed 's|\.\/|\$EXTERNAL_SOURCE_NESTED\/|g') $path|g" ./docker/Dockerfile; 
+for dockerfile in ./docker/Dockerfile .rhdh/docker/Dockerfile; do
+  sed -i "/# BEGIN COPY package.json files/,/# END COPY package.json files/c# BEGIN COPY package.json files\n# END COPY package.json files" $dockerfile
+  for path in $(find . -name package.json | grep -v node_modules/ ); do
+    sed -i "s|\# BEGIN COPY package.json files|\# BEGIN COPY package.json files\nCOPY ${path/\./\$EXTERNAL_SOURCE_NESTED} $path|g" $dockerfile
+  done
 done
