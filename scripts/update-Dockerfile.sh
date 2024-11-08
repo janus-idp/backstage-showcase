cbcde8da7dffa9baf45ca44d339b71408817115f
Nick Boldt <nboldt@redhat.com>
2024-11-08 13:53:47 -0400

    chore(docker): exclude dynamic-plugins-root/ paths from the list of COPY commands in the dockerfile (#1912)
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- scripts/update-Dockerfile.sh
+++ scripts/update-Dockerfile.sh
@@ -14,7 +14,7 @@ for dockerfile in ./docker/Dockerfile .rhdh/docker/Dockerfile; do
   # trim existing COPY lines
   sed -i "/# BEGIN COPY package.json files/,/# END COPY package.json files/c# BEGIN COPY package.json files\n# END COPY package.json files" $dockerfile
   # add new COPY lines
-  for path in $(find . -name package.json | grep -v node_modules/ | sort -uV); do
+  for path in $(find . -name package.json | grep -E -v "node_modules/|dynamic-plugins-root/" | sort -uV); do
     sed -i "s|\# BEGIN COPY package.json files|\# BEGIN COPY package.json files\nCOPY ${path/\./\$EXTERNAL_SOURCE_NESTED} $path|g" $dockerfile
   done
 done
