99ae4876b37c56893a3cda2d8d2a6fcb1242c20f
Kashish Mittal <kmittal@redhat.com>
2024-11-15 13:37:55 -0500

    chore: update quay-actions wrapper
    Signed-off-by: Kashish Mittal <kmittal@redhat.com>

--- plugins/dynamic-plugins-info/src/components/InternalPluginsMap.ts
+++ plugins/dynamic-plugins-info/src/components/InternalPluginsMap.ts
@@ -64,8 +64,8 @@ export const InternalPluginsMap: Record<string, string> = {
     './dynamic-plugins/dist/roadiehq-scaffolder-backend-module-utils-dynamic',
   'roadiehq-scaffolder-backend-module-http-request-dynamic':
     './dynamic-plugins/dist/roadiehq-scaffolder-backend-module-http-request-dynamic',
-  'janus-idp-backstage-scaffolder-backend-module-quay-dynamic':
-    './dynamic-plugins/dist/janus-idp-backstage-scaffolder-backend-module-quay-dynamic',
+  'backstage-community-plugin-scaffolder-backend-module-quay-dynamic':
+    './dynamic-plugins/dist/backstage-community-plugin-scaffolder-backend-module-quay-dynamic',
   'janus-idp-backstage-scaffolder-backend-module-regex-dynamic':
     './dynamic-plugins/dist/janus-idp-backstage-scaffolder-backend-module-regex-dynamic',
   'backstage-community-plugin-rbac':
