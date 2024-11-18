82176b0f6f21409d501d73530474e0162c0ec19e
Omar Al Jaljuli <oaljalju@redhat.com>
2024-11-18 09:34:40 -0500

    fix rebase mistakes

--- .ibm/pipelines/openshift-ci-tests.sh
+++ .ibm/pipelines/openshift-ci-tests.sh
@@ -428,16 +428,7 @@ initiate_deployments() {
   configure_namespace "${NAME_SPACE_POSTGRES_DB}"
   configure_namespace "${NAME_SPACE_RBAC}"
   configure_external_postgres_db "${NAME_SPACE_RBAC}"
-<<<<<<< HEAD
 
-=======
-  
-<<<<<<< HEAD
-=======
-  # Renable when namespace termination issue is solved
-  # install_pipelines_operator "${DIR}"
->>>>>>> cc79ac8e (add OCM cluster-role-binding to operator projects)
->>>>>>> b4da0ab9 (add OCM cluster-role-binding to operator projects)
   uninstall_helmchart "${NAME_SPACE_RBAC}" "${RELEASE_NAME_RBAC}"
   apply_yaml_files "${DIR}" "${NAME_SPACE_RBAC}" "${RELEASE_NAME_RBAC}"
   echo "Deploying image from repository: ${QUAY_REPO}, TAG_NAME: ${TAG_NAME}, in NAME_SPACE : ${RELEASE_NAME_RBAC}"
