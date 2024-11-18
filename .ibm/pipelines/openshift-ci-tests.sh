7f583dcf549a690a99f1e38dca60a2de7cda3f2e
Zbyněk Drápela <zbynek.drapela@gmail.com>
2024-11-18 10:42:11 +0100

    Rebase

--- .ibm/pipelines/openshift-ci-tests.sh
+++ .ibm/pipelines/openshift-ci-tests.sh
@@ -371,38 +371,6 @@ initiate_deployments() {
   helm upgrade -i "${RELEASE_NAME_RBAC}" -n "${NAME_SPACE_RBAC}" "${HELM_REPO_NAME}/${HELM_IMAGE_NAME}" --version "${CHART_VERSION}" -f "${DIR}/value_files/${HELM_CHART_RBAC_VALUE_FILE_NAME}" --set global.clusterRouterBase="${K8S_CLUSTER_ROUTER_BASE}" --set upstream.backstage.image.repository="${QUAY_REPO}" --set upstream.backstage.image.tag="${TAG_NAME}"
 }
 
-initiate_aks_deployment() {
-  add_helm_repos
-  install_helm
-  delete_namespace "${NAME_SPACE_RBAC_AKS}"
-  configure_namespace "${NAME_SPACE_AKS}"
-  # Renable when namespace termination issue is solved
-  # install_tekton_pipelines
-  uninstall_helmchart "${NAME_SPACE_AKS}" "${RELEASE_NAME}"
-  cd "${DIR}"
-  apply_yaml_files "${DIR}" "${NAME_SPACE_AKS}"
-  yq_merge_value_files "${DIR}/value_files/${HELM_CHART_VALUE_FILE_NAME}" "${DIR}/value_files/${HELM_CHART_AKS_DIFF_VALUE_FILE_NAME}" "/tmp/${HELM_CHART_AKS_MERGED_VALUE_FILE_NAME}"
-  cp -a "/tmp/${HELM_CHART_AKS_MERGED_VALUE_FILE_NAME}" "${ARTIFACT_DIR}/${NAME_SPACE_AKS}" # Save the final value-file into the artifacts directory.
-  echo "Deploying image from repository: ${QUAY_REPO}, TAG_NAME: ${TAG_NAME}, in NAME_SPACE: ${NAME_SPACE_AKS}"
-  helm upgrade -i "${RELEASE_NAME}" -n "${NAME_SPACE_AKS}" "${HELM_REPO_NAME}/${HELM_IMAGE_NAME}" --version "${CHART_VERSION}" -f "/tmp/${HELM_CHART_AKS_MERGED_VALUE_FILE_NAME}" --set global.host="${K8S_CLUSTER_ROUTER_BASE}" --set upstream.backstage.image.repository="${QUAY_REPO}" --set upstream.backstage.image.tag="${TAG_NAME}"
-}
-
-initiate_rbac_aks_deployment() {
-  add_helm_repos
-  install_helm
-  delete_namespace "${NAME_SPACE_AKS}"
-  configure_namespace "${NAME_SPACE_RBAC_AKS}"
-  # Renable when namespace termination issue is solved
-  # install_tekton_pipelines
-  uninstall_helmchart "${NAME_SPACE_RBAC_AKS}" "${RELEASE_NAME_RBAC}"
-  cd "${DIR}"
-  apply_yaml_files "${DIR}" "${NAME_SPACE_RBAC_AKS}"
-  yq_merge_value_files "${DIR}/value_files/${HELM_CHART_RBAC_VALUE_FILE_NAME}" "${DIR}/value_files/${HELM_CHART_RBAC_AKS_DIFF_VALUE_FILE_NAME}" "/tmp/${HELM_CHART_RBAC_AKS_MERGED_VALUE_FILE_NAME}"
-  cp -a "/tmp/${HELM_CHART_RBAC_AKS_MERGED_VALUE_FILE_NAME}" "${ARTIFACT_DIR}/${NAME_SPACE_RBAC_AKS}" # Save the final value-file into the artifacts directory.
-  echo "Deploying image from repository: ${QUAY_REPO}, TAG_NAME: ${TAG_NAME}, in NAME_SPACE: ${NAME_SPACE_RBAC_AKS}"
-  helm upgrade -i "${RELEASE_NAME_RBAC}" -n "${NAME_SPACE_RBAC_AKS}" "${HELM_REPO_NAME}/${HELM_IMAGE_NAME}" --version "${CHART_VERSION}" -f "/tmp/${HELM_CHART_RBAC_AKS_MERGED_VALUE_FILE_NAME}" --set global.host="${K8S_CLUSTER_ROUTER_BASE}" --set upstream.backstage.image.repository="${QUAY_REPO}" --set upstream.backstage.image.tag="${TAG_NAME}"
-}
-
 initiate_rds_deployment() {
   local release_name=$1
   local namespace=$2
@@ -415,41 +383,6 @@ initiate_rds_deployment() {
   oc apply -f "$DIR/resources/postgres-db/postgres-cred.yaml" -n "${namespace}"
   oc apply -f "$DIR/resources/postgres-db/dynamic-plugins-root-PVC.yaml" -n "${namespace}"
   helm upgrade -i "${release_name}" -n "${namespace}" "${HELM_REPO_NAME}/${HELM_IMAGE_NAME}" --version "${CHART_VERSION}" -f "$DIR/resources/postgres-db/values-showcase-postgres.yaml" --set global.clusterRouterBase="${K8S_CLUSTER_ROUTER_BASE}" --set upstream.backstage.image.repository="${QUAY_REPO}" --set upstream.backstage.image.tag="${TAG_NAME}"
-}
-
-initiate_gke_deployment() {
-  gcloud_ssl_cert_create $GKE_CERT_NAME $GKE_INSTANCE_DOMAIN_NAME $GOOGLE_CLOUD_PROJECT
-  add_helm_repos
-  install_helm
-  #delete_namespace "${NAME_SPACE_RBAC_AKS}"
-  configure_namespace "${NAME_SPACE_AKS}"
-  # Renable when namespace termination issue is solved
-  # install_tekton_pipelines
-  uninstall_helmchart "${NAME_SPACE_AKS}" "${RELEASE_NAME}"
-  cd "${DIR}"
-  apply_yaml_files "${DIR}" "${NAME_SPACE_AKS}"
-  oc apply -f "${DIR}/cluster/gke/frontend-config.yaml" --namespace="${project}"
-  yq_merge_value_files "${DIR}/value_files/${HELM_CHART_VALUE_FILE_NAME}" "${DIR}/value_files/${HELM_CHART_GKE_DIFF_VALUE_FILE_NAME}" "/tmp/${HELM_CHART_GKE_MERGED_VALUE_FILE_NAME}"
-  cp -a "/tmp/${HELM_CHART_GKE_MERGED_VALUE_FILE_NAME}" "${ARTIFACT_DIR}/${NAME_SPACE_AKS}" # Save the final value-file into the artifacts directory.
-  echo "Deploying image from repository: ${QUAY_REPO}, TAG_NAME: ${TAG_NAME}, in NAME_SPACE: ${NAME_SPACE_AKS}"
-  helm upgrade -i "${RELEASE_NAME}" -n "${NAME_SPACE_AKS}" "${HELM_REPO_NAME}/${HELM_IMAGE_NAME}" --version "${CHART_VERSION}" -f "/tmp/${HELM_CHART_GKE_MERGED_VALUE_FILE_NAME}" --set global.host="${K8S_CLUSTER_ROUTER_BASE}" --set upstream.backstage.image.repository="${QUAY_REPO}" --set upstream.backstage.image.tag="${TAG_NAME}"
-}
-
-initiate_rbac_gke_deployment() {
-  gcloud_ssl_cert_create $GKE_CERT_NAME $GKE_INSTANCE_DOMAIN_NAME $GOOGLE_CLOUD_PROJECT
-  add_helm_repos
-  install_helm
-  delete_namespace "${NAME_SPACE_AKS}"
-  configure_namespace "${NAME_SPACE_RBAC_AKS}"
-  # Renable when namespace termination issue is solved
-  # install_tekton_pipelines
-  uninstall_helmchart "${NAME_SPACE_RBAC_AKS}" "${RELEASE_NAME_RBAC}"
-  cd "${DIR}"
-  apply_yaml_files "${DIR}" "${NAME_SPACE_RBAC_AKS}"
-  yq_merge_value_files "${DIR}/value_files/${HELM_CHART_RBAC_VALUE_FILE_NAME}" "${DIR}/value_files/${HELM_CHART_RBAC_GKE_DIFF_VALUE_FILE_NAME}" "/tmp/${HELM_CHART_RBAC_GKE_MERGED_VALUE_FILE_NAME}"
-  cp -a "/tmp/${HELM_CHART_RBAC_AKS_MERGED_VALUE_FILE_NAME}" "${ARTIFACT_DIR}/${NAME_SPACE_RBAC_AKS}" # Save the final value-file into the artifacts directory.
-  echo "Deploying image from repository: ${QUAY_REPO}, TAG_NAME: ${TAG_NAME}, in NAME_SPACE: ${NAME_SPACE_RBAC_AKS}"
-  helm upgrade -i "${RELEASE_NAME_RBAC}" -n "${NAME_SPACE_RBAC_AKS}" "${HELM_REPO_NAME}/${HELM_IMAGE_NAME}" --version "${CHART_VERSION}" -f "/tmp/${HELM_CHART_RBAC_GKE_MERGED_VALUE_FILE_NAME}" --set global.host="${K8S_CLUSTER_ROUTER_BASE}" --set upstream.backstage.image.repository="${QUAY_REPO}" --set upstream.backstage.image.tag="${TAG_NAME}"
 }
 
 check_and_test() {
