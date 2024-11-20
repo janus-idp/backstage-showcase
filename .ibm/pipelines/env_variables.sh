0c33eefc94ffd8e3255bb16d305e05e9a10f92e9
Zbyněk Drápela <61500440+zdrapela@users.noreply.github.com>
2024-11-20 17:57:59 +0100

    chore(ci): add automated tests on GKE & fix AKS (#1918)
    * chore(ci): fix AKS e2e tests
    
    * Move `az` and edit AKS
    
    * Add gcloud CLI to Dockerfile
    
    * Add GKE
    
    * OVERRIDE JOB_NAME
    
    * Move helm set
    
    * Add GH auth app 3
    
    * GKE RBAC adapt to recent values file changes
    
    * Revert "GKE RBAC adapt to recent values file changes"
    
    This reverts commit 7c521d11a76f8713107eb279138edbda9ff3004d.
    
    * Better http_status
    
    * Save merged Value files
    
    * Move AKS and GKE deployments to separate files
    
    * Replace AKS and GKE namespace with K8S that unifies them
    
    * Better source for AKS and GKE sh
    
    * Fix domain name
    
    * Update diff-values_showcase-rbac_AKS.yaml
    
    * Fix configmap creation
    
    * Safer configmap
    
    * Rebase
    
    * Fix install helm position
    
    * fix sourcing for AKS GKE
    
    * JOB_NAME OVERRIDE
    
    * Revert "JOB_NAME OVERRIDE"
    
    This reverts commit f93bddd92f220bb3627b517e0174672e555e3ec6.
    
    * Resolve review comment

--- .ibm/pipelines/env_variables.sh
+++ .ibm/pipelines/env_variables.sh
@@ -4,10 +4,12 @@ set -a  # Automatically export all variables
 #ENVS and Vault Secrets
 HELM_CHART_VALUE_FILE_NAME="values_showcase.yaml"
 HELM_CHART_RBAC_VALUE_FILE_NAME="values_showcase-rbac.yaml"
+HELM_CHART_K8S_MERGED_VALUE_FILE_NAME="merged-values_showcase_K8S.yaml"
+HELM_CHART_RBAC_K8S_MERGED_VALUE_FILE_NAME="merged-values_showcase-rbac_K8S.yaml"
 HELM_CHART_AKS_DIFF_VALUE_FILE_NAME="diff-values_showcase_AKS.yaml"
 HELM_CHART_RBAC_AKS_DIFF_VALUE_FILE_NAME="diff-values_showcase-rbac_AKS.yaml"
-HELM_CHART_AKS_MERGED_VALUE_FILE_NAME="merged-values_showcase_AKS.yaml"
-HELM_CHART_RBAC_AKS_MERGED_VALUE_FILE_NAME="merged-values_showcase-rbac_AKS.yaml"
+HELM_CHART_GKE_DIFF_VALUE_FILE_NAME="diff-values_showcase_GKE.yaml"
+HELM_CHART_RBAC_GKE_DIFF_VALUE_FILE_NAME="diff-values_showcase-rbac_GKE.yaml"
 HELM_IMAGE_NAME=backstage
 HELM_REPO_NAME=rhdh-chart
 HELM_REPO_URL="https://redhat-developer.github.io/rhdh-chart"
@@ -29,6 +31,10 @@ GITHUB_APP_2_APP_ID=$(cat /tmp/secrets/GITHUB_APP_2_APP_ID)
 GITHUB_APP_2_CLIENT_ID=$(cat /tmp/secrets/GITHUB_APP_2_CLIENT_ID)
 GITHUB_APP_2_PRIVATE_KEY=$(cat /tmp/secrets/GITHUB_APP_2_PRIVATE_KEY)
 GITHUB_APP_2_CLIENT_SECRET=$(cat /tmp/secrets/GITHUB_APP_2_CLIENT_SECRET)
+GITHUB_APP_3_APP_ID=$(cat /tmp/secrets/GITHUB_APP_3_APP_ID)
+GITHUB_APP_3_CLIENT_ID=$(cat /tmp/secrets/GITHUB_APP_3_CLIENT_ID)
+GITHUB_APP_3_PRIVATE_KEY=$(cat /tmp/secrets/GITHUB_APP_3_PRIVATE_KEY)
+GITHUB_APP_3_CLIENT_SECRET=$(cat /tmp/secrets/GITHUB_APP_3_CLIENT_SECRET)
 GITHUB_APP_JANUS_TEST_APP_ID=OTE3NjM5
 GITHUB_APP_JANUS_TEST_CLIENT_ID=SXYyM2xpSEdtU1l6SUFEbHFIakw=
 GITHUB_APP_JANUS_TEST_PRIVATE_KEY=$(cat /tmp/secrets/GITHUB_APP_JANUS_TEST_PRIVATE_KEY)
@@ -91,5 +97,13 @@ ARM_CLIENT_ID=$(cat /tmp/secrets/ARM_CLIENT_ID)
 ARM_CLIENT_SECRET=$(cat /tmp/secrets/ARM_CLIENT_SECRET)
 AKS_NIGHTLY_CLUSTER_NAME=$(cat /tmp/secrets/AKS_NIGHTLY_CLUSTER_NAME)
 AKS_NIGHTLY_CLUSTER_RESOURCEGROUP=$(cat /tmp/secrets/AKS_NIGHTLY_CLUSTER_RESOURCEGROUP)
+AKS_INSTANCE_DOMAIN_NAME=$(cat /tmp/secrets/AKS_INSTANCE_DOMAIN_NAME)
+
+GKE_CLUSTER_NAME=$(cat /tmp/secrets/GKE_CLUSTER_NAME)
+GKE_CLUSTER_REGION=$(cat /tmp/secrets/GKE_CLUSTER_REGION)
+GKE_INSTANCE_DOMAIN_NAME=$(cat /tmp/secrets/GKE_INSTANCE_DOMAIN_NAME)
+GKE_SERVICE_ACCOUNT_NAME=$(cat /tmp/secrets/GKE_SERVICE_ACCOUNT_NAME)
+GKE_CERT_NAME=$(cat /tmp/secrets/GKE_CERT_NAME)
+GOOGLE_CLOUD_PROJECT=$(cat /tmp/secrets/GOOGLE_CLOUD_PROJECT)
 
 set +a  # Stop automatically exporting variables
