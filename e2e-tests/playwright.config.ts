b8803c713ebd12cc9497c0d1762c74b5e40187bb
Nil Gallego <github.i0l5z@simplelogin.com>
2024-11-27 11:18:21 +0100

    wip

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -1,14 +1,4 @@
 import { defineConfig, devices } from "@playwright/test";
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
-
 const useCommonDeviceAndViewportConfig = {
   use: {
     ...devices["Desktop Chrome"],
@@ -130,42 +120,5 @@ export default defineConfig({
       ...useCommonDeviceAndViewportConfig,
       testMatch: ["**/playwright/e2e/configuration-test/config-map.spec.ts"],
     },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-    //
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
