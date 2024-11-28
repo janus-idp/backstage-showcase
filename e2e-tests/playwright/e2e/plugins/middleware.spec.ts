9b4647da36eeb5615af4933ee56399212052b621
Nil Gallego <github.i0l5z@simplelogin.com>
2024-11-28 17:07:41 +0100

    fix url on middleware

--- e2e-tests/playwright/e2e/plugins/middleware.spec.ts
+++ e2e-tests/playwright/e2e/plugins/middleware.spec.ts
@@ -1,10 +1,14 @@
 // https://github.com/gashcrumb/dynamic-plugins-root-http-middleware/tree/main/plugins/middleware-header-example
 
 import test, { expect, request } from "@playwright/test";
+import playwrightConfig from "../../../playwright.config";
 
 test.only("Check the middleware is working", async () => {
-  const endpoint = "api/simple-chat";
-  const context = await request.newContext({ baseURL: endpoint });
+  const endpoint = playwrightConfig.use.baseURL + "/api/simple-chat";
+  console.log(`middleware endpoint full url: ${endpoint}`);
+  const context = await request.newContext({
+    baseURL: endpoint,
+  });
   const r = await context.get("");
   expect(r.headers["X-PROXY-TEST-HEADER"]);
 });
