66de823ca0ecfb829284c752908e90ed531618ac
albarbaro <abarbaro@redhat.com>
2024-11-28 12:24:15 +0100

    fix timeouts

--- e2e-tests/playwright/e2e/authProviders/rhsso-76-provider.spec.ts
+++ e2e-tests/playwright/e2e/authProviders/rhsso-76-provider.spec.ts
@@ -19,7 +19,7 @@ import { RhdhAuthHack } from "../../support/api/rhdh-auth-hack";
 
 let page: Page;
 
-for (const version of ["RHSSO"]) {
+for (const version of ["RHBK", "RHSSO"]) {
   test.describe(`Standard authentication providers: OIDC with ${version}`, () => {
     test.use({ baseURL: constants.AUTH_PROVIDERS_BASE_URL });
 
