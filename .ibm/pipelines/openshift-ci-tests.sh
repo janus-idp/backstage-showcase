019f8f25353617d52b39fbb1cd0fdea447a62214
Gustavo Lira <guga.java@gmail.com>
2024-11-29 20:06:57 -0300

    test
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>

--- .ibm/pipelines/openshift-ci-tests.sh
+++ .ibm/pipelines/openshift-ci-tests.sh
@@ -30,18 +30,18 @@ main() {
   . "${DIR}/env_variables.sh"
 
   if [[ "$JOB_NAME" == *aks* ]]; then
-      export K8S_CLUSTER_ROUTER_BASE="$AKS_INSTANCE_DOMAIN_NAME"
       az_login
+      export K8S_CLUSTER_ROUTER_BASE="$AKS_INSTANCE_DOMAIN_NAME"
       az_aks_start "${AKS_NIGHTLY_CLUSTER_NAME}" "${AKS_NIGHTLY_CLUSTER_RESOURCEGROUP}"
       az_aks_approuting_enable "${AKS_NIGHTLY_CLUSTER_NAME}" "${AKS_NIGHTLY_CLUSTER_RESOURCEGROUP}"
       az_aks_get_credentials "${AKS_NIGHTLY_CLUSTER_NAME}" "${AKS_NIGHTLY_CLUSTER_RESOURCEGROUP}"
     elif [[ "$JOB_NAME" == *gke* ]]; then
-      export K8S_CLUSTER_ROUTER_BASE="$GKE_INSTANCE_DOMAIN_NAME"
       gcloud_auth "${GKE_SERVICE_ACCOUNT_NAME}" "/tmp/secrets/GKE_SERVICE_ACCOUNT_KEY"
       gcloud_gke_get_credentials "${GKE_CLUSTER_NAME}" "${GKE_CLUSTER_REGION}" "${GOOGLE_CLOUD_PROJECT}"
+      export K8S_CLUSTER_ROUTER_BASE="$GKE_INSTANCE_DOMAIN_NAME"
     else
-      export K8S_CLUSTER_ROUTER_BASE=$(oc get route console -n openshift-console -o=jsonpath='{.spec.host}' | sed 's/^[^.]*\.//')
       oc login --token="${K8S_CLUSTER_TOKEN}" --server="${K8S_CLUSTER_URL}"
+      export K8S_CLUSTER_ROUTER_BASE=$(oc get route console -n openshift-console -o=jsonpath='{.spec.host}' | sed 's/^[^.]*\.//')
     fi
 
     echo "K8S_CLUSTER_ROUTER_BASE: $K8S_CLUSTER_ROUTER_BASE"
