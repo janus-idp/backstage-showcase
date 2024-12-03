bab03c6a94d4dd93ed70473b77dcad9744fd7dec
Subhash Khileri <skhileri@redhat.com>
2024-12-03 14:09:23 +0530

    fix mui v5 button locator (#2044)

--- e2e-tests/playwright/support/pageObjects/global-obj.ts
+++ e2e-tests/playwright/support/pageObjects/global-obj.ts
@@ -7,7 +7,7 @@ export const UIhelperPO = {
   // MUI v4 buttons are spans with a class starting with MuiButton-label
   // MUI v5 buttons are HTML buttons with a class containing MuiButton-root
   MuiButtonLabel:
-    'span[class^="MuiButton-label"],button[class~="MuiButton-root"]',
+    'span[class^="MuiButton-label"],button[class*="MuiButton-root"]',
   MuiToggleButtonLabel: 'span[class^="MuiToggleButton-label"]',
   MuiBoxLabel: 'div[class*="MuiBox-root"] label',
   MuiTableHead: 'th[class*="MuiTableCell-root"]',
