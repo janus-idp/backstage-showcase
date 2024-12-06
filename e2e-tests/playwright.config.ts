8d80488322a8433977017fe0ce854f451515bae8
Gustavo Lira e Silva <guga.java@gmail.com>
2024-12-06 14:13:13 -0300

    refactor: modularize OpenShift CI script for better maintainability - new (#2032)
    * modularize OpenShift CI script for better maintainability
    
    * Add log copying for auth providers in utils.sh script
    
    This commit introduces copying of logs from `auth-providers-logs` directory to the artifact directory. It ensures that logs for authentication providers are now included in the pipeline artifacts, aiding in troubleshooting and analysis. Additionally, the indentation for the screenshots copying block is corrected for consistency.

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -53,6 +53,7 @@ export default defineConfig({
         "**/playwright/e2e/plugins/bulk-import.spec.ts",
         "**/playwright/e2e/verify-tls-config-health-check.spec.ts",
         "**/playwright/e2e/configuration-test/config-map.spec.ts",
+        "**/playwright/e2e/plugins/tekton/tekton.spec.ts",
       ],
     },
     {
