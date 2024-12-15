0ec14acfb85a98f0963074b1475cc2176a0a4e82
Nil Gallego <github.i0l5z@simplelogin.com>
2024-12-15 05:42:31 +0100

    switch to keyloack, and add fixtures

--- e2e-tests/playwright/e2e/plugins/rbac/rbac-as-admin.spec.ts
+++ e2e-tests/playwright/e2e/plugins/rbac/rbac-as-admin.spec.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-empty-pattern */
-import test, { expect, Page } from "@playwright/test";
+import test, { expect } from "@playwright/test";
 import { Roles } from "../../../support/pages/rbac";
-import { setupBrowser, Common } from "../../../utils/common";
+import { Common } from "../../../utils/common";
 import { UIhelper } from "../../../utils/ui-helper";
 import { UI_HELPER_ELEMENTS } from "../../../support/pageObjects/global-obj";
 import {
@@ -15,20 +15,26 @@ type RbacAsAdminFixture = {
   uiHelper: UIhelper;
   rolesHelper: Roles;
   testId: { testId: string; testRole: string; composedRole: string };
+  rbacPo: RbacPo;
 };
 
-let myPage: Page;
+/*
+    Note that:
+    The policies generated from a policy.csv or ConfigMap file cannot be edited or deleted using the Developer Hub Web UI. 
+    https://docs.redhat.com/en/documentation/red_hat_developer_hub/1.2/html/authorization/proc-rbac-ui-manage-roles_title-authorization#proc-rbac-ui-edit-role_title-authorization
+*/
 
 const base = test.extend<RbacAsAdminFixture>({
   //prettier-ignore
-  uiHelper: async ({}, use) => { //NOSONAR
-    const uiHelper = new UIhelper(myPage);
-    await myPage.goto("/rbac");
+  uiHelper: async ({page}, use) => { //NOSONAR
+    const uiHelper = new UIhelper(page);
+    await new Common(page).loginAsKeycloakUser();
+    await page.goto("/rbac");
     await use(uiHelper);
   },
   //prettier-ignore
-  rolesHelper: async ({}, use) => { //NOSONAR
-    const rolesHelper = new Roles(myPage);
+  rolesHelper: async ({page}, use) => { //NOSONAR
+    const rolesHelper = new Roles(page);
     await use(rolesHelper);
   },
   //prettier-ignore
@@ -38,14 +44,13 @@ const base = test.extend<RbacAsAdminFixture>({
     const composedRole = `role:default/${testRole}`;
     await use({ testId, testRole, composedRole });
   },
+  //prettier-ignore
+  rbacPo: async ({page}, use) => { //NOSONAR
+    use( new RbacPo(page))
+  },
 });
 
 base.describe.serial("Test RBAC plugin as an admin user", () => {
-  base.beforeAll(async ({ browser }, testInfo) => {
-    myPage = (await setupBrowser(browser, testInfo)).page;
-    await new Common(myPage).loginAsGithubUser();
-  });
-
   base(
     "Check if Administration side nav is present with RBAC plugin",
     async ({ uiHelper }) => {
@@ -54,6 +59,38 @@ base.describe.serial("Test RBAC plugin as an admin user", () => {
       await uiHelper.verifyColumnHeading(allGridColumnsText);
       const allCellsIdentifier = Roles.getRolesListCellsIdentifier();
       await uiHelper.verifyCellsInTable(allCellsIdentifier);
+    },
+  );
+
+  base(
+    "Check if permission policies defined in files are loaded",
+    async ({ page }) => {
+      const uiHelper = new UIhelper(page);
+
+      const testRole: string = "role:default/test2-role";
+
+      await uiHelper.verifyHeading(/All roles \(\d+\)/);
+      await uiHelper.verifyLink(testRole);
+      await uiHelper.clickLink(testRole);
+
+      await uiHelper.verifyHeading(testRole);
+      await uiHelper.clickTab("Overview");
+
+      await uiHelper.verifyText("About");
+      await uiHelper.verifyText("csv permission policy file");
+
+      await uiHelper.verifyHeading("Users and groups (1 group");
+      await uiHelper.verifyHeading("Permission policies (2)");
+      const permissionPoliciesColumnsText =
+        Roles.getPermissionPoliciesListColumnsText();
+      await uiHelper.verifyColumnHeading(permissionPoliciesColumnsText);
+      const permissionPoliciesCellsIdentifier =
+        Roles.getPermissionPoliciesListCellsIdentifier();
+      await uiHelper.verifyCellsInTable(permissionPoliciesCellsIdentifier);
+
+      await expect(page.getByRole("article")).toContainText("catalog-entity");
+      await expect(page.getByRole("article")).toContainText("Read, Update");
+      await expect(page.getByRole("article")).toContainText("Delete");
     },
   );
 
@@ -83,21 +120,20 @@ base.describe.serial("Test RBAC plugin as an admin user", () => {
 
   base(
     "Create and edit a role from the roles list page",
-    async ({ uiHelper, rolesHelper }) => {
-      const rbacPo = new RbacPo(myPage);
+    async ({ uiHelper, rolesHelper, page, testId, rbacPo }) => {
       const testUser = "Jonathon Page";
-      await rbacPo.createRole("test-role", [
+      await rbacPo.createRole(testId.testRole, [
         RbacPo.rbacTestUsers.guest,
         RbacPo.rbacTestUsers.tara,
         RbacPo.rbacTestUsers.backstage,
       ]);
-      await myPage.click(
-        ROLES_PAGE_COMPONENTS.editRole("role:default/test-role"),
+      await page.click(
+        ROLES_PAGE_COMPONENTS.editRole("role:default/" + testId.testRole),
       );
       await uiHelper.verifyHeading("Edit Role");
       await uiHelper.clickButton("Next");
       await rbacPo.addUsersAndGroups(testUser);
-      await myPage.click(rbacPo.selectMember(testUser));
+      await page.click(rbacPo.selectMember(testUser));
       await uiHelper.verifyHeading("Users and groups (3 users, 1 group)");
       await uiHelper.clickButton("Next");
       await uiHelper.clickButton("Next");
@@ -106,95 +142,93 @@ base.describe.serial("Test RBAC plugin as an admin user", () => {
         "Role role:default/test-role updated successfully",
       );
 
-      await myPage
+      await page
         .locator(HOME_PAGE_COMPONENTS.searchBar)
         .waitFor({ state: "visible" });
-      await myPage.locator(HOME_PAGE_COMPONENTS.searchBar).fill("test-role");
+      await page.locator(HOME_PAGE_COMPONENTS.searchBar).fill(testId.testRole);
       await uiHelper.verifyHeading("All roles (1)");
-      const usersAndGroupsLocator = myPage
+      const usersAndGroupsLocator = page
         .locator(UI_HELPER_ELEMENTS.MuiTableCell)
         .filter({ hasText: "3 users, 1 group" });
       await usersAndGroupsLocator.waitFor();
       await expect(usersAndGroupsLocator).toBeVisible();
 
-      await rolesHelper.deleteRole("role:default/test-role");
+      await rolesHelper.deleteRole(testId.composedRole);
     },
   );
 
   base(
     "Edit users and groups and update policies of a role from the overview page",
-    async ({ uiHelper, rolesHelper }) => {
-      const rbacPo = new RbacPo(myPage);
-      await rbacPo.createRole("test-role1", [
+    async ({ uiHelper, rolesHelper, page, testId, rbacPo }) => {
+      await rbacPo.createRole(testId.testRole, [
         RbacPo.rbacTestUsers.guest,
         RbacPo.rbacTestUsers.tara,
         RbacPo.rbacTestUsers.backstage,
       ]);
 
-      await uiHelper.filterInputPlaceholder("test-role1");
+      await uiHelper.filterInputPlaceholder(testId.testRole);
 
-      await uiHelper.clickLink("role:default/test-role1");
+      await uiHelper.clickLink(testId.composedRole);
 
-      await uiHelper.verifyHeading("role:default/test-role1");
+      await uiHelper.verifyHeading(testId.composedRole);
       await uiHelper.clickTab("Overview");
 
-      await myPage.click(ROLE_OVERVIEW_COMPONENTS.updateMembers);
+      await page.click(ROLE_OVERVIEW_COMPONENTS.updateMembers);
       await uiHelper.verifyHeading("Edit Role");
-      await myPage
+      await page
         .locator(HOME_PAGE_COMPONENTS.searchBar)
         .fill("Guest User".toLowerCase());
-      await myPage.click('button[aria-label="Remove"]');
+      await page.click('button[aria-label="Remove"]');
       await uiHelper.verifyHeading("Users and groups (1 user, 1 group)");
       await uiHelper.clickButton("Next");
       await uiHelper.clickButton("Next");
       await uiHelper.clickButton("Save");
       await uiHelper.verifyText(
-        "Role role:default/test-role1 updated successfully",
+        `Role ${testId.composedRole} updated successfully`,
       );
       await uiHelper.verifyHeading("Users and groups (1 user, 1 group)");
 
-      await myPage.click(ROLE_OVERVIEW_COMPONENTS.updatePolicies);
+      await page.click(ROLE_OVERVIEW_COMPONENTS.updatePolicies);
       await uiHelper.verifyHeading("Edit Role");
       await rbacPo.clickAddPermissionPolicy();
-      await myPage.click(rbacPo.selectPermissionPolicyPlugin(1), {
+      await page.click(rbacPo.selectPermissionPolicyPlugin(1), {
         timeout: 10_1000,
       });
       await rbacPo.selectOption("scaffolder");
-      await myPage.click(rbacPo.selectPermissionPolicyPermission(1));
+      await page.click(rbacPo.selectPermissionPolicyPermission(1));
       await rbacPo.selectOption("scaffolder-template");
       await uiHelper.clickButton("Next");
       await uiHelper.clickButton("Save");
       await uiHelper.verifyText(
-        "Role role:default/test-role1 updated successfully",
+        `Role ${testId.composedRole} updated successfully`,
       );
       await uiHelper.verifyHeading("Permission Policies (3)");
 
-      await rolesHelper.deleteRole("role:default/test-role1");
+      await rolesHelper.deleteRole(testId.composedRole);
     },
   );
 
   base(
     "Create a role with a permission policy per resource type and verify that the only authorized users can access specific resources.",
-    async ({ uiHelper, rolesHelper }) => {
-      await new RbacPo(myPage).createRole(
-        "test-role",
+    async ({ uiHelper, rolesHelper, page, testId, rbacPo }) => {
+      await rbacPo.createRole(
+        testId.testRole,
         ["Guest User", "rhdh-qe", "Backstage"],
         "anyOf",
       );
 
-      await myPage
+      await page
         .locator(HOME_PAGE_COMPONENTS.searchBar)
         .waitFor({ state: "visible" });
-      await myPage.locator(HOME_PAGE_COMPONENTS.searchBar).fill("test-role");
+      await page.locator(HOME_PAGE_COMPONENTS.searchBar).fill(testId.testRole);
       await uiHelper.verifyHeading("All roles (1)");
-      await rolesHelper.deleteRole("role:default/test-role");
+      await rolesHelper.deleteRole(testId.composedRole);
     },
   );
 
   base(
     "Admin cannot create a role if there are no rules defined for the selected resource type.",
-    async ({ uiHelper, testId }) => {
-      const rbacPo = new RbacPo(myPage);
+    async ({ uiHelper, testId, page, rbacPo }) => {
       await uiHelper.clickButton("Create");
       await uiHelper.verifyHeading("Create role");
 
@@ -203,33 +237,30 @@ base.describe.serial("Test RBAC plugin as an admin user", () => {
       await rbacPo.addUsersAndGroups(RbacPo.rbacTestUsers.guest.toLowerCase());
       await rbacPo.clickSelectMember(RbacPo.rbacTestUsers.guest);
       await rbacPo.next();
-      const policy = myPage.locator(rbacPo.selectPermissionPolicyPlugin(0), {
+      const policy = page.locator(rbacPo.selectPermissionPolicyPlugin(0), {
         hasText: RbacPo.rbacTestUsers.guest,
       });
 
       await policy.first().click();
       await rbacPo.selectOption("catalog");
 
-      await myPage.click(rbacPo.selectPermissionPolicyPermission(0));
+      await page.click(rbacPo.selectPermissionPolicyPermission(0));
       await rbacPo.selectOption("catalog.entity.create");
 
-      await expect(myPage.getByLabel("configure-access")).not.toBeVisible();
+      await expect(page.getByLabel("configure-access")).not.toBeVisible();
       await uiHelper.clickButton("Cancel");
     },
   );
 
   base(
     "As an RHDH admin, I want to be able to restrict access by using the Not condition to part of the plugin, so that some information is protected from unauthorized access.",
-    async ({ uiHelper, testId, rolesHelper }) => {
-      const rbacPo = new RbacPo(myPage);
+    async ({ uiHelper, testId, rolesHelper, page, rbacPo }) => {
       // TODO: likely
       await rbacPo.createRole(testId.testRole, []);
-      await myPage
+      await page
         .locator(HOME_PAGE_COMPONENTS.searchBar)
         .waitFor({ state: "visible" });
-      await myPage
-        .locator(HOME_PAGE_COMPONENTS.searchBar)
-        .fill(testId.testRole);
+      await page.locator(HOME_PAGE_COMPONENTS.searchBar).fill(testId.testRole);
       await uiHelper.verifyHeading("All roles (1)");
 
       await rolesHelper.deleteRole(testId.composedRole);
@@ -238,30 +269,27 @@ base.describe.serial("Test RBAC plugin as an admin user", () => {
 
   base(
     "As an RHDH admin, I want to be able to edit the access rule, so I can keep it up to date and be able to add more plugins in the future.",
-    async ({ uiHelper, testId }) => {
-      const rbacPo = new RbacPo(myPage);
+    async ({ uiHelper, testId, page, rbacPo }) => {
       // TODO: likely
       await rbacPo.createRole(testId.testRole, []);
-      await myPage
+      await page
         .locator(HOME_PAGE_COMPONENTS.searchBar)
         .waitFor({ state: "visible" });
-      await myPage
-        .locator(HOME_PAGE_COMPONENTS.searchBar)
-        .fill(testId.testRole);
+      await page.locator(HOME_PAGE_COMPONENTS.searchBar).fill(testId.testRole);
       await uiHelper.verifyHeading("All roles (1)");
 
-      await myPage.click(ROLES_PAGE_COMPONENTS.editRole(testId.composedRole));
+      await page.click(ROLES_PAGE_COMPONENTS.editRole(testId.composedRole));
       await uiHelper.verifyHeading("Edit Role");
       await uiHelper.clickButton("Next");
       await uiHelper.clickButton("Next");
 
-      await myPage.getByLabel("configure-access").click();
-      await myPage.getByRole("button", { name: "Condition" }).click();
-      await myPage.getByTestId("rules-sidebar").getByLabel("Open").click();
-      await myPage.getByText("HAS_SPEC").click();
-      await myPage.getByLabel("key *").click();
-      await myPage.getByLabel("key *").fill("lifecycle");
-      await myPage.getByTestId("save-conditions").click();
+      await page.getByLabel("configure-access").click();
+      await page.getByRole("button", { name: "Condition" }).click();
+      await page.getByTestId("rules-sidebar").getByLabel("Open").click();
+      await page.getByText("HAS_SPEC").click();
+      await page.getByLabel("key *").click();
+      await page.getByLabel("key *").fill("lifecycle");
+      await page.getByTestId("save-conditions").click();
 
       await uiHelper.clickButton("Next");
       await uiHelper.clickButton("Save");
@@ -273,27 +301,23 @@ base.describe.serial("Test RBAC plugin as an admin user", () => {
 
   base(
     "As an RHDH admin, I want to be able to remove an access rule from an existing permission policy.",
-    async ({ uiHelper, testId }) => {
-      base.slow();
-      const rbacPo = new RbacPo(myPage);
+    async ({ uiHelper, testId, page, rbacPo }) => {
       // TODO: likely
       await rbacPo.createRole(testId.testRole, []);
-      await myPage
+      await page
         .locator(HOME_PAGE_COMPONENTS.searchBar)
         .waitFor({ state: "visible" });
-      await myPage
-        .locator(HOME_PAGE_COMPONENTS.searchBar)
-        .fill(testId.testRole);
+      await page.locator(HOME_PAGE_COMPONENTS.searchBar).fill(testId.testRole);
       await uiHelper.verifyHeading("All roles (1)");
-      await myPage.reload({ waitUntil: "domcontentloaded" });
-      await myPage.click(ROLES_PAGE_COMPONENTS.editRole(testId.composedRole));
+      await page.reload({ waitUntil: "domcontentloaded" });
+      await page.click(ROLES_PAGE_COMPONENTS.editRole(testId.composedRole));
       await uiHelper.verifyHeading("Edit Role");
       await uiHelper.clickButton("Next");
       await uiHelper.clickButton("Next");
 
-      await myPage.getByLabel("configure-access").click();
-      await myPage.getByRole("button", { name: "Remove" }).nth(2).click();
-      await myPage.getByTestId("save-conditions").click();
+      await page.getByLabel("configure-access").click();
+      await page.getByRole("button", { name: "Remove" }).nth(2).click();
+      await page.getByTestId("save-conditions").click();
       await uiHelper.verifyText("Configure access (2 rules)");
 
       await uiHelper.clickButton("Next");
--- e2e-tests/playwright/e2e/plugins/rbac/rbac-plugin.spec.ts
+++ e2e-tests/playwright/e2e/plugins/rbac/rbac-plugin.spec.ts
@@ -1,159 +1,164 @@
 import { Page, expect, test } from "@playwright/test";
 import { Roles } from "../../../support/pages/rbac";
-import { Common, setupBrowser } from "../../../utils/common";
+import { Common } from "../../../utils/common";
 import { UIhelper } from "../../../utils/ui-helper";
 import fs from "fs/promises";
 
-test.describe("Test RBAC plugin: load permission policies and conditions from files", () => {
-  let common: Common;
-  let uiHelper: UIhelper;
-  let page: Page;
+type RbacPluginFixture = {
+  uiHelper: UIhelper;
+};
 
-  test.beforeAll(async ({ browser }, testInfo) => {
-    page = (await setupBrowser(browser, testInfo)).page;
-
-    uiHelper = new UIhelper(page);
-    common = new Common(page);
-    await common.loginAsGithubUser();
-  });
-
-  test.beforeEach(async () => {
+const base = test.extend<RbacPluginFixture>({
+  //prettier-ignore
+  uiHelper: async ({page}, use) => { //NOSONAR
+    const uiHelper = new UIhelper(page);
+    await new Common(page).loginAsKeycloakUser();
     await page.goto("/rbac");
-    await new Common(page).checkAndClickOnGHloginPopup();
-  });
-
-  test("Should download the user list", async () => {
-    await page.locator('a:has-text("Download User List")').click();
-    const fileContent = await downloadAndReadFile(page);
-    const lines = fileContent.trim().split("\n");
-
-    const header = "userEntityRef,displayName,email,lastAuthTime";
-    if (lines[0] !== header) {
-      throw new Error("Header does not match");
-    }
-
-    // Check that each subsequent line starts with "user:default"
-    const allUsersValid = lines
-      .slice(1)
-      .every((line) => line.startsWith("user:default"));
-    if (!allUsersValid) {
-      throw new Error("Not all users info are valid");
-    }
-  });
-
-  async function downloadAndReadFile(page: Page): Promise<string | undefined> {
-    const [download] = await Promise.all([
-      page.waitForEvent("download"),
-      page.locator('a:has-text("Download User List")').click(),
-    ]);
-
-    const filePath = await download.path();
-
-    if (filePath) {
-      const fileContent = await fs.readFile(filePath, "utf-8");
-      return fileContent;
-    } else {
-      console.error("Download failed or path is not available");
-      return undefined;
-    }
-  }
-
-  test("Check if permission policies defined in files are loaded and effective", async () => {
-    const testRole: string = "role:default/test2-role";
-
-    await uiHelper.verifyHeading(/All roles \(\d+\)/);
-    await uiHelper.verifyLink(testRole);
-    await uiHelper.clickLink(testRole);
-
-    await uiHelper.verifyHeading(testRole);
-    await uiHelper.clickTab("Overview");
-
-    await uiHelper.verifyText("About");
-    await uiHelper.verifyText("csv permission policy file");
-
-    await uiHelper.verifyHeading("Users and groups (1 group");
-    await uiHelper.verifyHeading("Permission policies (2)");
-    const permissionPoliciesColumnsText =
-      Roles.getPermissionPoliciesListColumnsText();
-    await uiHelper.verifyColumnHeading(permissionPoliciesColumnsText);
-    const permissionPoliciesCellsIdentifier =
-      Roles.getPermissionPoliciesListCellsIdentifier();
-    await uiHelper.verifyCellsInTable(permissionPoliciesCellsIdentifier);
-
-    await expect(page.getByRole("article")).toContainText("catalog-entity");
-    await expect(page.getByRole("article")).toContainText("Read, Update");
-    await expect(page.getByRole("article")).toContainText("Delete");
-
-    await page.getByTestId("update-members").getByLabel("Update").click();
-    await expect(page.locator("tbody")).toContainText("rhdh-qe-2-team");
-    await uiHelper.clickButton("Next");
-    await page.getByLabel("configure-access").first().click();
-    await expect(page.getByPlaceholder("string, string")).toHaveValue(
-      "group:janus-qe/rhdh-qe-2-team,$currentUser",
-    );
-    await page.getByTestId("cancel-conditions").click();
-    await page.getByLabel("configure-access").nth(1).click();
-    await expect(page.getByPlaceholder("string, string")).toHaveValue(
-      "$currentUser",
-    );
-    await page.getByTestId("cancel-conditions").click();
-    await uiHelper.clickButton("Next");
-    await uiHelper.clickButton("Cancel");
-  });
-
-  test.afterAll(async () => {
-    await page.close();
-  });
+    await use(uiHelper);
+  },
 });
 
-test.describe("Test RBAC plugin: Aliases used in conditional access policies", () => {
-  let common: Common;
-  let uiHelper: UIhelper;
-  let page: Page;
+base.describe(
+  "Test RBAC plugin: load permission policies and conditions from files",
+  () => {
+    base.beforeEach(async ({ page }) => {
+      await new Common(page).loginAsKeycloakUser();
+      await page.goto("/rbac");
+    });
 
-  test.beforeAll(async ({ browser }, testInfo) => {
-    page = (await setupBrowser(browser, testInfo)).page;
+    base("Should download the user list", async ({ page }) => {
+      await page.locator('a:has-text("Download User List")').click();
+      const fileContent = await downloadAndReadFile(page);
+      const lines = fileContent.trim().split("\n");
 
-    uiHelper = new UIhelper(page);
-    common = new Common(page);
-    await common.loginAsGithubUser(process.env.GH_USER2_ID);
-  });
+      const header = "userEntityRef,displayName,email,lastAuthTime";
+      if (lines[0] !== header) {
+        throw new Error("Header does not match");
+      }
 
-  test.beforeEach(
-    async () => await new Common(page).checkAndClickOnGHloginPopup(),
-  );
+      // Check that each subsequent line starts with "user:default"
+      const allUsersValid = lines
+        .slice(1)
+        .every((line) => line.startsWith("user:default"));
+      if (!allUsersValid) {
+        throw new Error("Not all users info are valid");
+      }
+    });
 
-  test("Check if aliases used in conditions: the user is allowed to unregister only components they own, not those owned by the group.", async () => {
-    await uiHelper.openSidebar("Catalog");
-    await uiHelper.selectMuiBox("Kind", "Component");
+    async function downloadAndReadFile(
+      page: Page,
+    ): Promise<string | undefined> {
+      const [download] = await Promise.all([
+        page.waitForEvent("download"),
+        page.locator('a:has-text("Download User List")').click(),
+      ]);
 
-    await uiHelper.searchInputPlaceholder("test-rhdh-qe-2");
-    await page
-      .getByRole("link", { name: "test-rhdh-qe-2", exact: true })
-      .click();
+      const filePath = await download.path();
 
-    await expect(page.locator("header")).toContainText("user:rhdh-qe-2");
-    await page.getByTestId("menu-button").click();
-    const unregisterUserOwned = page.getByText("Unregister entity");
-    await expect(unregisterUserOwned).toBeEnabled();
+      if (filePath) {
+        const fileContent = await fs.readFile(filePath, "utf-8");
+        return fileContent;
+      } else {
+        console.error("Download failed or path is not available");
+        return undefined;
+      }
+    }
 
-    await page.getByText("Unregister entity").click();
-    await expect(page.getByRole("heading")).toContainText(
-      "Are you sure you want to unregister this entity?",
+    base(
+      "Check if permission policies defined in files are loaded and effective",
+      async ({ page, uiHelper }) => {
+        const testRole: string = "role:default/test2-role";
+
+        await uiHelper.verifyHeading(/All roles \(\d+\)/);
+        await uiHelper.verifyLink(testRole);
+        await uiHelper.clickLink(testRole);
+
+        await uiHelper.verifyHeading(testRole);
+        await uiHelper.clickTab("Overview");
+
+        await uiHelper.verifyText("About");
+        await uiHelper.verifyText("csv permission policy file");
+
+        await uiHelper.verifyHeading("Users and groups (1 group");
+        await uiHelper.verifyHeading("Permission policies (2)");
+        const permissionPoliciesColumnsText =
+          Roles.getPermissionPoliciesListColumnsText();
+        await uiHelper.verifyColumnHeading(permissionPoliciesColumnsText);
+        const permissionPoliciesCellsIdentifier =
+          Roles.getPermissionPoliciesListCellsIdentifier();
+        await uiHelper.verifyCellsInTable(permissionPoliciesCellsIdentifier);
+
+        await expect(page.getByRole("article")).toContainText("catalog-entity");
+        await expect(page.getByRole("article")).toContainText("Read, Update");
+        await expect(page.getByRole("article")).toContainText("Delete");
+
+        await page.getByTestId("update-members").getByLabel("Update").click();
+        await expect(page.locator("tbody")).toContainText("rhdh-qe-2-team");
+        await uiHelper.clickButton("Next");
+        await page.getByLabel("configure-access").first().click();
+        await expect(page.getByPlaceholder("string, string")).toHaveValue(
+          "group:janus-qe/rhdh-qe-2-team,$currentUser",
+        );
+        await page.getByTestId("cancel-conditions").click();
+        await page.getByLabel("configure-access").nth(1).click();
+        await expect(page.getByPlaceholder("string, string")).toHaveValue(
+          "$currentUser",
+        );
+        await page.getByTestId("cancel-conditions").click();
+        await uiHelper.clickButton("Next");
+        await uiHelper.clickButton("Cancel");
+      },
     );
-    await page.getByRole("button", { name: "Cancel" }).click();
+  },
+);
 
-    await uiHelper.openSidebar("Catalog");
-    await page.getByRole("link", { name: "test-rhdh-qe-2-team-owned" }).click();
-    await expect(page.locator("header")).toContainText(
-      "janus-qe/rhdh-qe-2-team",
-    );
-    await page.getByTestId("menu-button").click();
-    const unregisterGroupOwned = page.getByText("Unregister entity");
-    await expect(unregisterGroupOwned).toBeDisabled();
-  });
-
-  test.afterAll(async () => {
-    await page.close();
-  });
+const baseUser2 = test.extend<RbacPluginFixture>({
+  //prettier-ignore
+  uiHelper: async ({page}, use) => { //NOSONAR
+    const uiHelper = new UIhelper(page);
+    await new Common(page).loginAsKeycloakUser(  process.env.GH_USER2_ID,
+      process.env.GH_USER2_PASS,);
+    await page.goto("/rbac");
+    await use(uiHelper);
+  },
 });
+
+baseUser2.describe(
+  "Test RBAC plugin: Aliases used in conditional access policies",
+  () => {
+    baseUser2(
+      "Check if aliases used in conditions: the user is allowed to unregister only components they own, not those owned by the group.",
+      async ({ uiHelper, page }) => {
+        await uiHelper.openSidebar("Catalog");
+        await uiHelper.selectMuiBox("Kind", "Component");
+
+        await uiHelper.searchInputPlaceholder("test-rhdh-qe-2");
+        await page
+          .getByRole("link", { name: "test-rhdh-qe-2", exact: true })
+          .click();
+
+        await expect(page.locator("header")).toContainText("user:rhdh-qe-2");
+        await page.getByTestId("menu-button").click();
+        const unregisterUserOwned = page.getByText("Unregister entity");
+        await expect(unregisterUserOwned).toBeEnabled();
+
+        await page.getByText("Unregister entity").click();
+        await expect(page.getByRole("heading")).toContainText(
+          "Are you sure you want to unregister this entity?",
+        );
+        await page.getByRole("button", { name: "Cancel" }).click();
+
+        await uiHelper.openSidebar("Catalog");
+        await page
+          .getByRole("link", { name: "test-rhdh-qe-2-team-owned" })
+          .click();
+        await expect(page.locator("header")).toContainText(
+          "janus-qe/rhdh-qe-2-team",
+        );
+        await page.getByTestId("menu-button").click();
+        const unregisterGroupOwned = page.getByText("Unregister entity");
+        await expect(unregisterGroupOwned).toBeDisabled();
+      },
+    );
+  },
+);
