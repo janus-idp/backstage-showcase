7bb7197b4acc9a605a5030cfb657881e59e25cad
Omar Al Jaljuli <oaljalju@redhat.com>
2024-12-16 14:11:52 -0500

    chore(e2e): fix tech radar tests for nightly (#2111)

--- .ibm/pipelines/jobs/periodic.sh
+++ .ibm/pipelines/jobs/periodic.sh
@@ -5,10 +5,9 @@ handle_nightly() {
 
   export K8S_CLUSTER_ROUTER_BASE=$(oc get route console -n openshift-console -o=jsonpath='{.spec.host}' | sed 's/^[^.]*\.//')
 
-  configure_namespace "${NAME_SPACE}"
-  deploy_test_backstage_provider "${NAME_SPACE}"
   cluster_setup
   initiate_deployments
+  deploy_test_backstage_provider "${NAME_SPACE}"
   local url="https://${RELEASE_NAME}-backstage-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}"
   check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "${url}"
   local rbac_url="https://${RELEASE_NAME_RBAC}-backstage-${NAME_SPACE_RBAC}.${K8S_CLUSTER_ROUTER_BASE}"
