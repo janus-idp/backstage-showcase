2a705ebf1dbdd9a72eb9725f202819cd3ad36d68
Gustavo Lira <guga.java@gmail.com>
2024-12-19 13:16:42 -0300

    test
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>

--- .ibm/pipelines/jobs/main.sh
+++ .ibm/pipelines/jobs/main.sh
@@ -13,4 +13,15 @@ handle_main() {
   check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "${url}"
   local rbac_url="https://${RELEASE_NAME_RBAC}-backstage-${NAME_SPACE_RBAC}.${K8S_CLUSTER_ROUTER_BASE}"
   check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC}" "${rbac_url}"
+
+  # Deploy `showcase-runtime` to run tests that require configuration changes at runtime
+    configure_namespace "${NAME_SPACE_RUNTIME}"
+    uninstall_helmchart "${NAME_SPACE_RUNTIME}" "${RELEASE_NAME}"
+    oc apply -f "$DIR/resources/redis-cache/redis-deployment.yaml" --namespace="${NAME_SPACE_RUNTIME}"
+
+    local runtime_url="https://${RELEASE_NAME}-backstage-${NAME_SPACE_RUNTIME}.${K8S_CLUSTER_ROUTER_BASE}"
+
+    apply_yaml_files "${DIR}" "${NAME_SPACE_RUNTIME}" "${runtime_url}"
+    helm upgrade -i "${RELEASE_NAME}" -n "${NAME_SPACE_RUNTIME}" "${HELM_REPO_NAME}/${HELM_IMAGE_NAME}" --version "${CHART_VERSION}" -f "${DIR}/value_files/${HELM_CHART_VALUE_FILE_NAME}" --set global.clusterRouterBase="${K8S_CLUSTER_ROUTER_BASE}" --set upstream.backstage.image.repository="${QUAY_REPO}" --set upstream.backstage.image.tag="${TAG_NAME}"
+    check_and_test "${RELEASE_NAME}" "${NAME_SPACE_RUNTIME}" "${runtime_url}"
 }
