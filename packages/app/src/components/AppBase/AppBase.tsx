868b54f7f9a84d3aa96f1a5bbf7fcaf756a34a8f
Christoph Jerolimov <jerolimov+git@redhat.com>
2024-12-20 07:54:55 +0100

    fix(theme): fix issue that mui was loaded multiple times
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>

--- packages/app/src/components/AppBase/AppBase.tsx
+++ packages/app/src/components/AppBase/AppBase.tsx
@@ -20,6 +20,15 @@ import { ScaffolderFieldExtensions } from '@backstage/plugin-scaffolder-react';
 import { SearchPage as BackstageSearchPage } from '@backstage/plugin-search';
 import { UserSettingsPage } from '@backstage/plugin-user-settings';
 
+import ButtonV4 from '@material-ui/core/Button';
+import GridV4 from '@material-ui/core/Grid';
+import TextFieldV4 from '@material-ui/core/TextField';
+import AutocompleteV4 from '@material-ui/lab/Autocomplete';
+import AutocompleteV5 from '@mui/material/Autocomplete';
+import ButtonV5 from '@mui/material/Button';
+import GridV5 from '@mui/material/Grid';
+import TextFieldV5 from '@mui/material/TextField';
+
 import { entityPage } from '../catalog/EntityPage';
 import DynamicRootContext from '../DynamicRoot/DynamicRootContext';
 import { LearningPaths } from '../learningPaths/LearningPathsPage';
@@ -27,6 +36,109 @@ import { Root } from '../Root';
 import ConfigUpdater from '../Root/ConfigUpdater';
 import { SearchPage } from '../search/SearchPage';
 import { settingsPage } from '../UserSettings/SettingsPages';
+
+const ButtonTest = () => {
+  const movies = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 },
+    { title: 'The Dark Knight', year: 2008 },
+    { title: '12 Angry Men', year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: 'Pulp Fiction', year: 1994 },
+    { title: 'The Lord of the Rings: The Return of the King', year: 2003 },
+    { title: 'The Good, the Bad and the Ugly', year: 1966 },
+    { title: 'Fight Club', year: 1999 },
+  ];
+
+  return (
+    <div>
+      <h1>Material UI v4 buttons</h1>
+      <div>
+        <ButtonV4 variant="contained">Default</ButtonV4>
+        <ButtonV4 variant="contained" color="primary">
+          Primary
+        </ButtonV4>
+        <ButtonV4 variant="contained" color="secondary">
+          Secondary
+        </ButtonV4>
+        <ButtonV4 variant="contained" disabled>
+          Disabled
+        </ButtonV4>
+        <ButtonV4 variant="contained" color="primary" href="#contained-buttons">
+          Link
+        </ButtonV4>
+      </div>
+
+      <h1>MUI v5 buttons</h1>
+      <div>
+        <ButtonV5 variant="contained">Default</ButtonV5>
+        <ButtonV5 variant="contained" color="primary">
+          Primary
+        </ButtonV5>
+        <ButtonV5 variant="contained" color="secondary">
+          Secondary
+        </ButtonV5>
+        <ButtonV5 variant="contained" disabled>
+          Disabled
+        </ButtonV5>
+        <ButtonV5 variant="contained" color="primary" href="#contained-buttons">
+          Link
+        </ButtonV5>
+      </div>
+
+      <h1>Material UI v4 Autocomplete</h1>
+      <div>
+        <AutocompleteV4
+          options={movies}
+          renderInput={params => <TextFieldV4 {...params} label="Movie" />}
+          getOptionLabel={option => option.title}
+        />
+      </div>
+
+      <h1>MUI v5 Autocomplete</h1>
+      <div>
+        <AutocompleteV5
+          options={movies}
+          renderInput={params => <TextFieldV5 {...params} label="Movie" />}
+          getOptionLabel={option => option.title}
+        />
+      </div>
+
+      <h1>MUI v4 Grid</h1>
+      <GridV4 container>
+        <GridV4 item>
+          <div style={{ backgroundColor: 'gray' }}>1</div>
+        </GridV4>
+        <GridV4 item>
+          <div style={{ backgroundColor: 'gray' }}>2</div>
+        </GridV4>
+        <GridV4 item>
+          <div style={{ backgroundColor: 'gray' }}>3</div>
+        </GridV4>
+        <GridV4 item>
+          <div style={{ backgroundColor: 'gray' }}>4</div>
+        </GridV4>
+      </GridV4>
+
+      <h1>MUI v5 Grid</h1>
+      <GridV5 container>
+        <GridV5 item>
+          <div style={{ backgroundColor: 'gray' }}>1</div>
+        </GridV5>
+        <GridV5 item>
+          <div style={{ backgroundColor: 'gray' }}>2</div>
+        </GridV5>
+        <GridV5 item>
+          <div style={{ backgroundColor: 'gray' }}>3</div>
+        </GridV5>
+        <GridV5 item>
+          <div style={{ backgroundColor: 'gray' }}>4</div>
+        </GridV5>
+      </GridV5>
+    </div>
+  );
+};
 
 const AppBase = () => {
   const {
@@ -120,6 +232,7 @@ const AppBase = () => {
             <Route path="/settings" element={<UserSettingsPage />}>
               {settingsPage}
             </Route>
+            <Route path="/button-test" element={<ButtonTest />} />
             <Route path="/catalog-graph" element={<CatalogGraphPage />} />
             <Route path="/learning-paths" element={<LearningPaths />} />
             {dynamicRoutes.map(
