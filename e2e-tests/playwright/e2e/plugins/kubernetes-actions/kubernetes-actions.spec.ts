077a9994812fd88670b000e26f3ba27dd6ed720d
Dominika Zemanovicova <dzemanov@redhat.com>
2025-01-07 11:13:28 +0100

    Handle namespaces more safely
    Signed-off-by: Dominika Zemanovicova <dzemanov@redhat.com>

--- e2e-tests/playwright/e2e/plugins/kubernetes-actions/kubernetes-actions.spec.ts
+++ e2e-tests/playwright/e2e/plugins/kubernetes-actions/kubernetes-actions.spec.ts
@@ -7,6 +7,7 @@ test.describe("Test Kubernetes Actions plugin", () => {
   let common: Common;
   let uiHelper: UIhelper;
   let kubeClient: KubeClient;
+  let namespace: string;
 
   test.beforeAll(async ({ browser }, testInfo) => {
     const page = (await setupBrowser(browser, testInfo)).page;
@@ -19,18 +20,20 @@ test.describe("Test Kubernetes Actions plugin", () => {
   });
 
   test("Creates kubernetes namespace", async () => {
-    const newNamespace = "test-kubernetes-actions-namespace";
+    namespace = `test-kubernetes-actions-${Date.now()}`;
     await uiHelper.verifyHeading("Software Templates");
     await uiHelper.clickBtnInCard("Create a kubernetes namespace", "Choose");
     await uiHelper.waitForTitle("Create a kubernetes namespace", 2);
 
-    await uiHelper.fillTextInputByLabel("Namespace name", newNamespace);
+    await uiHelper.fillTextInputByLabel("Namespace name", namespace);
     await uiHelper.fillTextInputByLabel("Url", process.env.K8S_CLUSTER_URL);
     await uiHelper.fillTextInputByLabel("Token", process.env.K8S_CLUSTER_TOKEN);
     await uiHelper.clickButton("Review");
     await uiHelper.clickButton("Create");
+    await kubeClient.getNamespaceByName(namespace);
+  });
 
-    await kubeClient.getNamespaceByName(newNamespace);
-    await kubeClient.deleteNamespaceAndWait(newNamespace);
+  test.afterEach(async () => {
+    await kubeClient.deleteNamespaceAndWait(namespace);
   });
 });
