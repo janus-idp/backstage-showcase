acadaf39da07aac5daab989eab2681d8008dd671
Dominika Zemanovicova <dzemanov@redhat.com>
2025-01-07 09:06:55 +0100

    Use KubeClient
    Signed-off-by: Dominika Zemanovicova <dzemanov@redhat.com>

--- e2e-tests/playwright/e2e/plugins/kubernetes-actions/kubernetes-actions.spec.ts
+++ e2e-tests/playwright/e2e/plugins/kubernetes-actions/kubernetes-actions.spec.ts
@@ -1,23 +1,25 @@
 import { test } from "@playwright/test";
-import { execSync } from "child_process";
 import { Common, setupBrowser } from "../../../utils/common";
 import { UIhelper } from "../../../utils/ui-helper";
+import { KubeClient } from "../../../utils/kube-client";
 
 test.describe("Test Kubernetes Actions plugin", () => {
   let common: Common;
   let uiHelper: UIhelper;
+  let kubeClient: KubeClient;
 
   test.beforeAll(async ({ browser }, testInfo) => {
     const page = (await setupBrowser(browser, testInfo)).page;
     common = new Common(page);
     uiHelper = new UIhelper(page);
+    kubeClient = new KubeClient();
 
     await common.loginAsGuest();
     await uiHelper.openSidebar("Create...");
   });
 
   test("Creates kubernetes namespace", async () => {
-    const newNamespace = "test-namespace";
+    const newNamespace = "test-kubernetes-actions-namespace";
     await uiHelper.verifyHeading("Software Templates");
     await uiHelper.clickBtnInCard("Create a kubernetes namespace", "Choose");
     await uiHelper.waitForTitle("Create a kubernetes namespace", 2);
@@ -28,24 +30,7 @@ test.describe("Test Kubernetes Actions plugin", () => {
     await uiHelper.clickButton("Review");
     await uiHelper.clickButton("Create");
 
-    try {
-      execSync(
-        `oc login --token="${process.env.K8S_CLUSTER_TOKEN}" --server="${process.env.K8S_CLUSTER_URL}"`,
-      );
-    } catch (error) {
-      throw new Error("Unable to login to Kubernetes cluster");
-    }
-
-    try {
-      execSync(`oc get namespace ${newNamespace}`);
-    } catch (error) {
-      throw new Error("Namespace not created");
-    }
-
-    try {
-      execSync(`oc delete namespace ${newNamespace}`);
-    } catch (error) {
-      throw new Error("Unable to clean up namespace");
-    }
+    await kubeClient.getNamespaceByName(newNamespace);
+    await kubeClient.deleteNamespaceAndWait(newNamespace);
   });
 });
