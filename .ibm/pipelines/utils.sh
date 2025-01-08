c0f032c137f3fb5bd80d3b32bc0c7fd3139fb146
Zbyněk Drápela <61500440+zdrapela@users.noreply.github.com>
2025-01-08 18:01:07 +0100

    chore(ci): improvements & fix for Data Router  (#2157)
    * chore(ci): Data Router improvements & fix
    
    * Add missing PR os credentials
    
    * Hide errors until last try
    
    * Nexus change

--- .ibm/pipelines/utils.sh
+++ .ibm/pipelines/utils.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 retrieve_pod_logs() {
   local pod_name=$1; local container=$2; local namespace=$3
@@ -36,67 +36,109 @@ save_all_pod_logs(){
 }
 
 droute_send() {
-  # Skipping ReportPortal for nightly jobs on OCP v4.14 and v4.13 for now, as new clusters are not behind the RH VPN.
-  if [[ "$JOB_NAME" == *ocp-v4* ]]; then
-    return 0
-  fi
+  if [[ "${OPENSHIFT_CI}" != "true" ]]; then return 0; fi
+  temp_kubeconfig=$(mktemp) # Create temporary KUBECONFIG to open second `oc` session
+  ( # Open subshell
+    if [ -n "${PULL_NUMBER:-}" ]; then
+      set +e
+    fi
+    export KUBECONFIG="$temp_kubeconfig"
+    local droute_version="1.2.2"
+    local release_name=$1
+    local project=$2
+    local droute_project="droute"
+    local metadata_output="data_router_metadata_output.json"
 
-  if [ -n "${PULL_NUMBER:-}" ]; then
-    set +e
-  fi
+    oc login --token="${RHDH_PR_OS_CLUSTER_TOKEN}" --server="${RHDH_PR_OS_CLUSTER_URL}"
+    oc whoami --show-server
+    local droute_pod_name=$(oc get pods -n droute --no-headers -o custom-columns=":metadata.name" | grep ubi9-cert-rsync)
+    local temp_droute=$(oc exec -n "${droute_project}" "${droute_pod_name}" -- /bin/bash -c "mktemp -d")
 
-  local droute_version="1.2"
-  local release_name=$1
-  local project=$2
-  local droute_project="droute"
-  local droute_pod_name=$(oc get pods -n droute --no-headers -o custom-columns=":metadata.name" | grep ubi9-cert-rsync)
-  METEDATA_OUTPUT="data_router_metadata_output.json"
+    JOB_BASE_URL="https://prow.ci.openshift.org/view/gs/test-platform-results"
+    if [ -n "${PULL_NUMBER:-}" ]; then
+      JOB_URL="${JOB_BASE_URL}/pr-logs/pull/${REPO_OWNER}_${REPO_NAME}/${PULL_NUMBER}/${JOB_NAME}/${BUILD_ID}"
+      ARTIFACTS_URL="https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/test-platform-results/pr-logs/pull/${REPO_OWNER}_${REPO_NAME}/${PULL_NUMBER}/${JOB_NAME}/${BUILD_ID}/artifacts/e2e-tests/${REPO_OWNER}-${REPO_NAME}/artifacts/${project}"
+    else
+      JOB_URL="${JOB_BASE_URL}/logs/${JOB_NAME}/${BUILD_ID}"
+      ARTIFACTS_URL="https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/test-platform-results/logs/${JOB_NAME}/${BUILD_ID}/artifacts/${JOB_NAME##periodic-ci-janus-idp-backstage-showcase-main-}/${REPO_OWNER}-${REPO_NAME}/artifacts/${project}"
+    fi
 
-  # Remove properties (only used for skipped test and invalidates the file if empty)
-  sed -i '/<properties>/,/<\/properties>/d' "${ARTIFACT_DIR}/${project}/${JUNIT_RESULTS}"
+    # Remove properties (only used for skipped test and invalidates the file if empty)
+    sed -i '/<properties>/,/<\/properties>/d' "${ARTIFACT_DIR}/${project}/${JUNIT_RESULTS}"
+    # Replace attachments with link to OpenShift CI storage
+    sed -iE "s#\[\[ATTACHMENT|\(.*\)\]\]#${ARTIFACTS_URL}/\1#g" "${ARTIFACT_DIR}/${project}/${JUNIT_RESULTS}"
 
-  JOB_BASE_URL="https://prow.ci.openshift.org/view/gs/test-platform-results"
-  if [ -n "${PULL_NUMBER:-}" ]; then
-    JOB_URL="${JOB_BASE_URL}/pr-logs/pull/${REPO_OWNER}_${REPO_NAME}/${PULL_NUMBER}/${JOB_NAME}/${BUILD_ID}"
-  else
-    JOB_URL="${JOB_BASE_URL}/logs/${JOB_NAME}/${BUILD_ID}"
-  fi
+    jq \
+      --arg hostname "$REPORTPORTAL_HOSTNAME" \
+      --arg project "$DATA_ROUTER_PROJECT" \
+      --arg name "$JOB_NAME" \
+      --arg description "[View job run details](${JOB_URL})" \
+      --arg key1 "job_type" \
+      --arg value1 "$JOB_TYPE" \
+      --arg key2 "pr" \
+      --arg value2 "$GIT_PR_NUMBER" \
+      --arg key3 "job_name" \
+      --arg value3 "$JOB_NAME" \
+      --arg key4 "tag_name" \
+      --arg value4 "$TAG_NAME" \
+      --arg auto_finalization_treshold $DATA_ROUTER_AUTO_FINALIZATION_TRESHOLD \
+      '.targets.reportportal.config.hostname = $hostname |
+      .targets.reportportal.config.project = $project |
+      .targets.reportportal.processing.launch.name = $name |
+      .targets.reportportal.processing.launch.description = $description |
+      .targets.reportportal.processing.launch.attributes += [
+          {"key": $key1, "value": $value1},
+          {"key": $key2, "value": $value2},
+          {"key": $key3, "value": $value3},
+          {"key": $key4, "value": $value4}
+        ] |
+      .targets.reportportal.processing.tfa.auto_finalization_threshold = ($auto_finalization_treshold | tonumber)
+      ' data_router/data_router_metadata_template.json > "${ARTIFACT_DIR}/${project}/${metadata_output}"
 
-  jq \
-    --arg hostname "$REPORTPORTAL_HOSTNAME" \
-    --arg project "$DATA_ROUTER_PROJECT" \
-    --arg name "$JOB_NAME" \
-    --arg description "[View job run details](${JOB_URL})" \
-    --arg key1 "job_type" \
-    --arg value1 "$JOB_TYPE" \
-    --arg key2 "pr" \
-    --arg value2 "$GIT_PR_NUMBER" \
-    --arg auto_finalization_treshold $DATA_ROUTER_AUTO_FINALIZATION_TRESHOLD \
-    '.targets.reportportal.config.hostname = $hostname |
-     .targets.reportportal.config.project = $project |
-     .targets.reportportal.processing.launch.name = $name |
-     .targets.reportportal.processing.launch.description = $description |
-     .targets.reportportal.processing.launch.attributes += [
-        {"key": $key1, "value": $value1},
-        {"key": $key2, "value": $value2}
-      ] |
-     .targets.reportportal.processing.tfa.auto_finalization_threshold = ($auto_finalization_treshold | tonumber)
-     ' data_router/data_router_metadata_template.json > "${ARTIFACT_DIR}/${project}/${METEDATA_OUTPUT}"
+    oc rsync --progress=true --include="${metadata_output}" --include="${JUNIT_RESULTS}" --exclude="*" -n "${droute_project}" "${ARTIFACT_DIR}/${project}/" "${droute_project}/${droute_pod_name}:${temp_droute}/"
 
-  oc rsync -n "${droute_project}" "${ARTIFACT_DIR}/${project}/" "${droute_project}/${droute_pod_name}:/tmp/droute"
+    # "Install" Data Router
+    oc exec -n "${droute_project}" "${droute_pod_name}" -- /bin/bash -c "
+      curl -fsSLk -o /tmp/droute-linux-amd64 'https://${DATA_ROUTER_NEXUS_HOSTNAME}/nexus/repository/dno-raw/droute-client/${droute_version}/droute-linux-amd64' \
+      && chmod +x /tmp/droute-linux-amd64 \
+      && /tmp/droute-linux-amd64 version"
 
-  oc exec -n "${droute_project}" "${droute_pod_name}" -- /bin/bash -c "
-    curl -fsSLk -o /tmp/droute-linux-amd64 'https://${NEXUS_HOSTNAME}/nexus/repository/dno-raw/droute-client/${droute_version}/droute-linux-amd64' && chmod +x /tmp/droute-linux-amd64"
+    # Send test results through DataRouter and save the request ID.
+    local max_attempts=5
+    local wait_seconds=1
+    for ((i = 1; i <= max_attempts; i++)); do
+      echo "Attempt ${i} of ${max_attempts} to send test results through Data Router."
+      if output=$(oc exec -n "${droute_project}" "${droute_pod_name}" -- /bin/bash -c "
+        /tmp/droute-linux-amd64 send --metadata ${temp_droute}/${metadata_output} \
+        --url '${DATA_ROUTER_URL}' \
+        --username '${DATA_ROUTER_USERNAME}' \
+        --password '${DATA_ROUTER_PASSWORD}' \
+        --results '${temp_droute}/${JUNIT_RESULTS}' \
+        --verbose" 2>&1); then
+        if DATA_ROUTER_REQUEST_ID=$(echo "$output" | grep "request:" | awk '{print $2}') &&
+          [ -n "$DATA_ROUTER_REQUEST_ID" ]; then
+          echo "Test results successfully sent through Data Router."
+          echo "Request ID: $DATA_ROUTER_REQUEST_ID"
+          break
+        fi
+      fi
 
-  oc exec -n "${droute_project}" "${droute_pod_name}" -- /bin/bash -c "
-    /tmp/droute-linux-amd64 send --metadata /tmp/droute/${METEDATA_OUTPUT} \
-    --url '${DATA_ROUTER_URL}' \
-    --username '${DATA_ROUTER_USERNAME}' \
-    --password '${DATA_ROUTER_PASSWORD}' \
-    --results '/tmp/droute/${JUNIT_RESULTS}' \
-    --verbose"
-
-  echo "exiting droute_send with exit code: $?"
-  set -e
-
+      if ((i == max_attempts)); then
+        echo "Failed to send test results after ${max_attempts} attempts."
+        echo "Last Data Router error details:"
+        echo "${output}"
+        echo "Troubleshooting steps:"
+        echo "1. Restart $droute_pod_name in $droute_project project/namespace"
+        echo "2. Check the Data Router documentation: https://spaces.redhat.com/pages/viewpage.action?pageId=115488042"
+        echo "3. Ask for help at Slack: #forum-dno-datarouter"
+      fi
+    done
+    
+    oc exec -n "${droute_project}" "${droute_pod_name}" -- /bin/bash -c "rm -rf ${temp_droute}/*"
+    if [ -n "${PULL_NUMBER:-}" ]; then
+      set -e
+    fi
+  ) # Close subshell
+  rm -f "$temp_kubeconfig" # Destroy temporary KUBECONFIG
+  oc whoami --show-server
 }
