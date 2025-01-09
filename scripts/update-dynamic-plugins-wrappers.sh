d15ddea125c4fc475d6be54b8292d0139192b6bf
Nick Boldt <nboldt@redhat.com>
2025-01-09 12:07:23 -0500

    also update dependencies; make script smarter too
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- scripts/update-dynamic-plugins-wrappers.sh
+++ scripts/update-dynamic-plugins-wrappers.sh
@@ -13,10 +13,27 @@
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo "")
 
+usage () {
+	echo "
+Usage: 
+  $0 [--minor] [--force]
+  
+Options:
+    --minor    also do minor updates (default: patch updates only)
+    --force    re-apply changes even if the version is the same
+"
+}
+
 DO_MINOR=0
-if [[ $1 == "--minor" ]] || [[ $BRANCH == "main" ]]; then 
-  DO_MINOR=1
-fi
+FORCE=0
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    '--minor') DO_MINOR=1; shift 1;;
+    '--force') FORCE=1; shift 1;;
+    *) echo "Unsupported flag '$1'"; usage; exit 1;
+  esac
+done
 
 for j in ./dynamic-plugins/wrappers/*/package.json; do
   (( c = c + 1 ))
@@ -24,8 +41,8 @@ done
 for j in ./dynamic-plugins/wrappers/*/package.json; do
   (( i = i + 1 ))
   echo
-  echo "[$i/$c] Diff $j ..."
   Plugin=$(jq -r '.name' "$j")
+  echo -n "[$i/$c] Diff $j ($Plugin / "
   VersionXYZ=$(jq -r '.version' "$j")
   VersionXY=${VersionXYZ%.*}
   if [[ $Plugin != "@"* ]]; then 
@@ -35,7 +52,7 @@ for j in ./dynamic-plugins/wrappers/*/package.json; do
         -e 's|backstage/community-|backstage-community/|' \
         -e 's|parfuemerie/douglas-|parfuemerie-douglas/|')"
   fi
-
+  echo "$Plugin) ..."
   allVersionsPublished="$(curl -sSLko- "https://registry.npmjs.org/${Plugin/\//%2f}" | jq -r '.versions[].version')"
   # echo $allVersionsPublished
   # clean out any pre-release versions
@@ -47,9 +64,10 @@ for j in ./dynamic-plugins/wrappers/*/package.json; do
     latestRelease="$(echo "$allVersionsPublished" | grep -v -E -- "next|alpha|-" | grep -E "^${VersionXY}" | sort -uV | tail -1)"
   fi
   # echo "[DEBUG] Latest x.y version at https://registry.npmjs.org/${Plugin/\//%2f} : $latestRelease"
-  if [[ "$latestRelease" != "$VersionXYZ" ]]; then
+  if [[ "$latestRelease" != "$VersionXYZ" ]] || [[ $FORCE -eq 1 ]]; then
     if [[ $i -gt 9 ]]; then echo -n " "; fi
     echo "       Bump $VersionXYZ -> $latestRelease from https://www.npmjs.com/package/$Plugin/v/$latestRelease"
     jq '.version = "'"$latestRelease"'"' "$j" > "$j"_; mv -f "$j"_ "$j"
+    sed -i "$j" -r -e 's|("'"$Plugin"'": )".+"|\1"'"$latestRelease"'"|'
   fi
 done
