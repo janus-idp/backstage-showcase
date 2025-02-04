33889da468cd879b89280d84bca0939c0baf4cb1
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-02-04 09:56:43 +0100

    chore(ci): extend AKS & GKE waiting for deployment

--- .ibm/pipelines/jobs/aks.sh
+++ .ibm/pipelines/jobs/aks.sh
@@ -8,13 +8,13 @@ handle_aks() {
   export NAME_SPACE_K8S="showcase-k8s-ci-nightly"
   export NAME_SPACE_RBAC_K8S="showcase-rbac-k8s-ci-nightly"
 
-  url="https://${K8S_CLUSTER_ROUTER_BASE}"
-
+  local url="https://${K8S_CLUSTER_ROUTER_BASE}"
   initiate_aks_deployment
-  check_and_test "${RELEASE_NAME}" "${NAME_SPACE_K8S}" "${url}" 50 30
+  check_and_test "${RELEASE_NAME}" "${NAME_SPACE_K8S}" "${url}" 50 30 20
   delete_namespace "${NAME_SPACE_K8S}"
-  initiate_rbac_aks_deployment
+
   local rbac_rhdh_base_url="https://${K8S_CLUSTER_ROUTER_BASE}"
-  check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC_K8S}" "${rbac_rhdh_base_url}"
+  initiate_rbac_aks_deployment
+  check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC_K8S}" "${rbac_rhdh_base_url}" 50 30 20
   delete_namespace "${NAME_SPACE_RBAC_K8S}"
 }
