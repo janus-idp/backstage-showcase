a3fc8c109cfc28eb44692803e4c69aec4b6c98c2
Alessandro Barbarossa <abarbaro@redhat.com>
2025-02-07 09:02:25 +0100

    Update utils.sh

--- .ibm/pipelines/utils.sh
+++ .ibm/pipelines/utils.sh
@@ -514,7 +514,7 @@ deploy_test_backstage_provider() {
   if ! oc get buildconfig test-backstage-customization-provider -n "${project}" >/dev/null 2>&1; then
     echo "Creating new app for test-backstage-customization-provider"
     oc new-app -S openshift/nodejs:18-minimal-ubi8
-    oc new-app https://github.com/janus-qe/test-backstage-customization-provider --image-stream="openshift/nodejs:18-minimal-ubi8" --namespace="${project}"
+    oc new-app https://github.com/janus-qe/test-backstage-customization-provider --image-stream="openshift/nodejs:18-ubi8" --namespace="${project}"
   else
     echo "BuildConfig for test-backstage-customization-provider already exists in ${project}. Skipping new-app creation."
   fi
