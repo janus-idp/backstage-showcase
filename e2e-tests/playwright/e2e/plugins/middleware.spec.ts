fd75beb97dc905a6bccc120e56391d540aa42218
Nil Gallego <github.i0l5z@simplelogin.com>
2025-02-10 11:24:41 +0100

    wip

--- e2e-tests/playwright/e2e/plugins/middleware.spec.ts
+++ e2e-tests/playwright/e2e/plugins/middleware.spec.ts
@@ -1,9 +1,18 @@
 // https://github.com/gashcrumb/dynamic-plugins-root-http-middleware/tree/main/plugins/middleware-header-example
 
-import test from "@playwright/test";
+import test, { request } from "@playwright/test";
 import { Common } from "../../utils/common";
+import { LOGGER } from "../../utils/logger";
 
 test.only("Check the middleware is working", async ({ page }) => {
   const common = new Common(page);
   await common.loginAsGuest();
+  const context = await request.newContext();
+  const response1 = await context.get("/add-test-header");
+  const response2 = await context.get("/api/simple-chat");
+  LOGGER.info(response1.headers());
+  LOGGER.info(response2.headers());
+  console.log(response1.headers());
+  console.log(response2.headers());
+  await page.goto("/add-test-header");
 });
