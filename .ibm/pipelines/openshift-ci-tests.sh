2a7fb805be1c5532d61af666da6105f1c5c5b03c
Gustavo Lira <guga.java@gmail.com>
2025-02-13 19:13:09 -0300

    Enable e2e tests
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>

--- .ibm/pipelines/openshift-ci-tests.sh
+++ .ibm/pipelines/openshift-ci-tests.sh
@@ -42,9 +42,9 @@ for SCRIPT in "${SCRIPTS[@]}"; do
     echo "Loaded ${SCRIPT}"
 done
 
-export K8S_CLUSTER_URL='https://api.alxdq5slv4a572c9df.eastus.aroapp.io:6443'
-export K8S_CLUSTER_TOKEN=${K8S_CLUSTER_TOKEN_TEMPORARY}
-export JOB_NAME=pull
+#export K8S_CLUSTER_URL='https://api.alxdq5slv4a572c9df.eastus.aroapp.io:6443'
+#export K8S_CLUSTER_TOKEN=${K8S_CLUSTER_TOKEN_TEMPORARY}
+#export JOB_NAME=pull
 
 main() {
   echo "Log file: ${LOGFILE}"
