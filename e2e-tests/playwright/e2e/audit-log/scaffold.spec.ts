2f58aff49eb2e3f26f8e92a40fc65acb5cc488db
Gustavo Lira <guga.java@gmail.com>
2025-02-14 11:50:18 -0300

    Refactor audit log tests to improve readability
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>

--- e2e-tests/playwright/e2e/audit-log/scaffold.spec.ts
+++ e2e-tests/playwright/e2e/audit-log/scaffold.spec.ts
@@ -25,59 +25,56 @@ test.describe("Audit Log check for Catalog Plugin", () => {
     await uiHelper.openSidebar("Create");
   });
 
-  test(
-    "Should fetch logs for ScaffolderParameterSchemaFetch event and validate log structure and values",
-    async ({ baseURL }) => {
-      await uiHelper.clickButton("Register Existing Component");
-      await catalogImport.registerExistingComponent(template, false);
-      await uiHelper.openSidebar("Create");
-      await common.waitForLoad();
-      await uiHelper.searchInputAriaLabel("Hello World 2");
-      await uiHelper.pressTab();
-      await uiHelper.clickButton("Choose");
+  test("Should fetch logs for ScaffolderParameterSchemaFetch event and validate log structure and values", async ({
+    baseURL,
+  }) => {
+    await uiHelper.clickButton("Register Existing Component");
+    await catalogImport.registerExistingComponent(template, false);
+    await uiHelper.openSidebar("Create");
+    await common.waitForLoad();
+    await uiHelper.searchInputAriaLabel("Hello World 2");
+    await uiHelper.pressTab();
+    await uiHelper.clickButton("Choose");
 
-      await LogUtils.validateLogEvent(
-        "ScaffolderParameterSchemaFetch",
-        "user:development/guest successfully requested the parameter schema for Template:default/hello-world-2",
-        "GET",
-        "/api/scaffolder/v2/templates/default/template/hello-world-2/parameter-schema",
-        baseURL!,
-        "scaffolder",
-      );
-    },
-  );
+    await LogUtils.validateLogEvent(
+      "ScaffolderParameterSchemaFetch",
+      "user:development/guest successfully requested the parameter schema for Template:default/hello-world-2",
+      "GET",
+      "/api/scaffolder/v2/templates/default/template/hello-world-2/parameter-schema",
+      baseURL!,
+      "scaffolder",
+    );
+  });
 
-  test(
-    "Should fetch logs for ScaffolderInstalledActionsFetch event and validate log structure and values",
-    async ({ baseURL }) => {
-      await uiHelper.clickById("long-menu");
-      await uiHelper.clickSpanByText("Installed Actions");
+  test("Should fetch logs for ScaffolderInstalledActionsFetch event and validate log structure and values", async ({
+    baseURL,
+  }) => {
+    await uiHelper.clickById("long-menu");
+    await uiHelper.clickSpanByText("Installed Actions");
 
-      await LogUtils.validateLogEvent(
-        "ScaffolderInstalledActionsFetch",
-        "user:development/guest requested the list of installed actions",
-        "GET",
-        "/api/scaffolder/v2/actions",
-        baseURL!,
-        "scaffolder",
-      );
-    },
-  );
+    await LogUtils.validateLogEvent(
+      "ScaffolderInstalledActionsFetch",
+      "user:development/guest requested the list of installed actions",
+      "GET",
+      "/api/scaffolder/v2/actions",
+      baseURL!,
+      "scaffolder",
+    );
+  });
 
-  test(
-    "Should fetch logs for ScaffolderTaskListFetch event and validate log structure and values",
-    async ({ baseURL }) => {
-      await uiHelper.clickById("long-menu");
-      await uiHelper.clickSpanByText("Task List");
+  test("Should fetch logs for ScaffolderTaskListFetch event and validate log structure and values", async ({
+    baseURL,
+  }) => {
+    await uiHelper.clickById("long-menu");
+    await uiHelper.clickSpanByText("Task List");
 
-      await LogUtils.validateLogEvent(
-        "ScaffolderTaskListFetch",
-        "user:development/guest successfully requested for the list of scaffolder tasks",
-        "GET",
-        "/api/scaffolder/v2/tasks?createdBy=user%3Adevelopment%2Fguest",
-        baseURL!,
-        "scaffolder",
-      );
-    },
-  );
+    await LogUtils.validateLogEvent(
+      "ScaffolderTaskListFetch",
+      "user:development/guest successfully requested for the list of scaffolder tasks",
+      "GET",
+      "/api/scaffolder/v2/tasks?createdBy=user%3Adevelopment%2Fguest",
+      baseURL!,
+      "scaffolder",
+    );
+  });
 });
