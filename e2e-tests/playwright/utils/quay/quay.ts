bf4ef038baf9db3ad4009ab21ed087bf5a03e671
Gustavo Lira <guga.java@gmail.com>
2025-02-17 15:30:41 -0300

    Refactor regex formatting in Quay utility function.
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>

--- e2e-tests/playwright/utils/quay/quay.ts
+++ e2e-tests/playwright/utils/quay/quay.ts
@@ -31,7 +31,9 @@ export class ImageRegistry {
     const securityScan = ["Critical", "High", "Medium", "Low", "Unknown"].map(
       (i) => `(${i}:\\s\\d+[^\\w]*)`,
     );
-    return new RegExp(`^(Passed|unsupported|Queued|(?:${securityScan.join("|")})+)$`);
+    return new RegExp(
+      `^(Passed|unsupported|Queued|(?:${securityScan.join("|")})+)$`,
+    );
   }
 
   static getAllScanColumnsText() {
