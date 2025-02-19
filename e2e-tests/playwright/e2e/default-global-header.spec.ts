c82cc2230e1d4edff078b79f9114185c1e1092b4
Yi Cai <yicai@redhat.com>
2025-02-19 16:42:16 -0500

    added header search bar test
    Signed-off-by: Yi Cai <yicai@redhat.com>

--- e2e-tests/playwright/e2e/default-global-header.spec.ts
+++ e2e-tests/playwright/e2e/default-global-header.spec.ts
@@ -21,6 +21,7 @@ test.describe("Default Global Header", () => {
         "Support (external link), Opens in a new window",
       ),
     ).toBeTruthy();
+    expect(await uiHelper.isLinkVisibleByLabel("Notifications")).toBeTruthy();
     expect(await uiHelper.isBtnVisible("Guest")).toBeTruthy();
   });
 
@@ -65,5 +66,20 @@ test.describe("Default Global Header", () => {
     await profileDropdown.click();
     await uiHelper.clickPbyText("Logout");
     await uiHelper.verifyHeading("Select a sign-in method");
+  });
+
+  test("Verify Search bar behaves as expected", async ({ page }) => {
+    const searchBar = page.locator(`input[placeholder="Search..."]`);
+    await searchBar.click();
+    await searchBar.fill("test query term");
+    expect(await uiHelper.isBtnVisibleByTitle("Clear")).toBeTruthy();
+    const dropdownList = page.locator(`ul[role="listbox"]`);
+    expect(await dropdownList.isVisible()).toBeTruthy();
+    await searchBar.press("Enter");
+    await uiHelper.verifyHeading("Search");
+    const searchResultPageInput = page.locator(
+      `input[id="search-bar-text-field"]`,
+    );
+    await expect(searchResultPageInput).toHaveValue("test query term");
   });
 });
