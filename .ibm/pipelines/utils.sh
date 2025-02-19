60252bbaffd1cb32c3e453261ab99d4bb3031577
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-02-19 18:55:44 +0100

    Add condition

--- .ibm/pipelines/utils.sh
+++ .ibm/pipelines/utils.sh
@@ -173,6 +173,9 @@ droute_send() {
       set -e
     fi
     oc exec -n "${droute_project}" "${droute_pod_name}" -- /bin/bash -c "rm -rf ${temp_droute}/*"
+    if [ -n "${PULL_NUMBER:-}" ]; then
+      set +e
+    fi
   ) # Close subshell
   rm -f "$temp_kubeconfig" # Destroy temporary KUBECONFIG
   oc whoami --show-server
